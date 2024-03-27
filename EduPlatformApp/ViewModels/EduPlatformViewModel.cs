d9f4af820e45440321b0424daea9bf141f0c8dcb
HoliakaDmytro <holidim39@gmail.com>
2024-03-27 13:13:08 +0200

    Add a ViewModel to store the aggregation of all ViewModels

---
+++ EduPlatformApp/ViewModels/EduPlatformViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace EduPlatform.WPF.ViewModels
+{
+    public class EduPlatformViewModel : ViewModelBase
+    {
+        public NavigationViewModel NavigationViewModel { get; }
+        public EduPlatformOverviewViewModel EduPlatformOverviewViewModel { get; }
+        public EduPlatformGroupsViewModel EduPlatformGroupsViewModel { get; }
+        public EduPlatformStudentsViewModel EduPlatformStudentsViewModel { get; }
+        public EduPlatformTeachersViewModel EduPlatformTeachersViewModel { get; }
+
+        public EduPlatformViewModel()
+        {
+            EduPlatformOverviewViewModel = new();
+            EduPlatformGroupsViewModel = new();
+            EduPlatformStudentsViewModel = new();
+            EduPlatformTeachersViewModel = new();
+            NavigationViewModel = new();
+        }
+    }
+}
