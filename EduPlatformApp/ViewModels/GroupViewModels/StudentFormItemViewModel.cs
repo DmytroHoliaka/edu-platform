873d11ab1913cb895adcb6394d72a69f9d8cec91
HoliakaDmytro <holidim39@gmail.com>
2024-03-29 14:06:37 +0200

    ViewModels: Add view model for student in adding group form

---
+++ EduPlatformApp/ViewModels/GroupViewModels/StudentFormItemViewModel.cs
@@ -0,0 +1,38 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using EduPlatform.WPF.ViewModels.GeneralViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace EduPlatform.WPF.ViewModels.GroupViewModels
+{
+    public class StudentFormItemViewModel : ViewModelBase
+    {
+        public string? FullName { get; }
+
+        public bool IsChecked
+        {
+            get
+            {
+                return _isChecked;
+            }
+            set
+            {
+                _isChecked = value;
+                OnPropertyChanged(nameof(IsChecked));
+            }
+        }
+
+        private bool _isChecked { get; set; }
+
+
+        public StudentFormItemViewModel(string fullName, bool isSelected = false)
+        {
+            FullName = fullName;
+            IsChecked = isSelected;
+        }
+    }
+}
