2475a411fabb12224d641feff762370553a96602
HoliakaDmytro <holidim39@gmail.com>
2024-03-30 13:10:01 +0200

    ViewModels: Add MainViewModel

---
+++ EduPlatformApp/ViewModels/GeneralViewModels/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿using EduPlatform.WPF.Service;
+using EduPlatform.WPF.Stores;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduPlatform.WPF.ViewModels.GeneralViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public EduPlatformViewModel EduPlatformViewModel { get; }
+        private ModalNavigationStore _modalNavigationStore { get; }
+
+        public MainViewModel(EduPlatformViewModel eduPlatformViewModel, ModalNavigationStore modalNavigationStore)
+        {
+            EduPlatformViewModel = eduPlatformViewModel;
+            _modalNavigationStore = modalNavigationStore;
+        }
+    }
+}
