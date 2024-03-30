2eeb545341c048e7f3046a0049d3b7d66eabf46c
HoliakaDmytro <holidim39@gmail.com>
2024-03-30 13:11:22 +0200

    ViewModels: Add general view model for updating group

---
+++ EduPlatformApp/ViewModels/GroupViewModels/UpdateGroupViewModel.cs
@@ -0,0 +1,19 @@
+﻿using EduPlatform.WPF.ViewModels.GroupViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduPlatform.WPF.ViewModels
+{
+    public class UpdateGroupViewModel
+    {
+        public GroupDetailsFormViewModel GroupDetailsFormViewModel { get; }
+
+        public UpdateGroupViewModel()
+        {
+            GroupDetailsFormViewModel = new();
+        }
+    }
+}
