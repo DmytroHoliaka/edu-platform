f02856b1b349ccb91a220b0b6bd0786e1dace28d
HoliakaDmytro <holidim39@gmail.com>
2024-03-30 13:11:54 +0200

    General: Update application class with new view models

--- EduPlatformApp/App.xaml.cs
+++ EduPlatformApp/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using EduPlatform.WPF.ViewModels.GeneralViewModels;
+﻿using EduPlatform.WPF.Service;
+using EduPlatform.WPF.Stores;
+using EduPlatform.WPF.ViewModels.GeneralViewModels;
 using System.Windows;
 
 namespace EduPlatform.WPF
@@ -8,11 +10,21 @@ namespace EduPlatform.WPF
     /// </summary>
     public partial class App : Application
     {
+        private readonly ModalNavigationStore _modalNavigationStore;
+        private readonly EduPlatformViewModel _eduPlatformViewModel;
+
+        public App()
+        {
+            _modalNavigationStore = new();
+            _eduPlatformViewModel = new();
+        }
+
         protected override void OnStartup(StartupEventArgs e)
         {
             MainWindow = new MainWindow()
             {
-                DataContext = new EduPlatformViewModel()
+                DataContext = new MainViewModel(_eduPlatformViewModel,
+                                                _modalNavigationStore)
             };
 
             MainWindow.Show();
