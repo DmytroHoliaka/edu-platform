da405ff0f6b73f810a86da7790190c8f4a2776af
HoliakaDmytro <holidim39@gmail.com>
2024-03-30 13:10:34 +0200

    ViewModels: Add general view model for creating group

---
+++ EduPlatformApp/ViewModels/GroupViewModels/CreateGroupViewModel.cs
@@ -0,0 +1,19 @@
+﻿using EduPlatform.WPF.ViewModels.GeneralViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduPlatform.WPF.ViewModels.GroupViewModels
+{
+    public class CreateGroupViewModel : ViewModelBase
+    {
+        public GroupDetailsFormViewModel GroupDetailsFormViewModel { get; }
+
+        public CreateGroupViewModel()
+        {
+            GroupDetailsFormViewModel = new();
+        }
+    }
+}
