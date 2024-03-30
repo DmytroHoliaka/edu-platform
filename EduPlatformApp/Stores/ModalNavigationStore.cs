fdcfc41c9a56e7c526fdab14b3102ef3d098653b
HoliakaDmytro <holidim39@gmail.com>
2024-03-30 13:09:44 +0200

    Stores: Add ModalNavigationStore

---
+++ EduPlatformApp/Stores/ModalNavigationStore.cs
@@ -0,0 +1,26 @@
+﻿using EduPlatform.WPF.ViewModels.GeneralViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduPlatform.WPF.Stores
+{
+    public class ModalNavigationStore
+    {
+        public ViewModelBase? CurrentViewModel
+        {
+            get => _currentViewModel;
+            set
+            {
+                _currentViewModel = value;
+                CurrentViewModelChanged?.Invoke();
+            }
+        }
+
+        public event Action? CurrentViewModelChanged;
+
+        private ViewModelBase? _currentViewModel;
+    }
+}
