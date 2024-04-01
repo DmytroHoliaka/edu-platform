e20e5f01b1bac2b0da9d9a829351c786809256d0
HoliakaDmytro <holidim39@gmail.com>
2024-04-01 18:10:20 +0300

    ViewModels: Complete MainViewModel

--- EduPlatformApp/ViewModels/GeneralViewModels/MainViewModel.cs
+++ EduPlatformApp/ViewModels/GeneralViewModels/MainViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using EduPlatform.WPF.Service;
 using EduPlatform.WPF.Stores;
+using EduPlatform.WPF.ViewModels.GroupViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,12 +12,30 @@ namespace EduPlatform.WPF.ViewModels.GeneralViewModels
     public class MainViewModel : ViewModelBase
     {
         public EduPlatformViewModel EduPlatformViewModel { get; }
-        private ModalNavigationStore _modalNavigationStore { get; }
+        private readonly ModalNavigationStore _modalNavigationStore;
+
+        public ViewModelBase? CurrentModalViewModel => _modalNavigationStore.CurrentViewModel;
+        public bool IsModalOpen => _modalNavigationStore.IsOpen;
+
 
         public MainViewModel(EduPlatformViewModel eduPlatformViewModel, ModalNavigationStore modalNavigationStore)
         {
             EduPlatformViewModel = eduPlatformViewModel;
             _modalNavigationStore = modalNavigationStore;
+
+            _modalNavigationStore.CurrentViewModelChanged += ModalNavigationStore_CurrentViewModelChanged;
+        } 
+
+        protected override void Dispose()
+        {
+            _modalNavigationStore.CurrentViewModelChanged -= ModalNavigationStore_CurrentViewModelChanged;
+            base.Dispose();
+        }
+
+        private void ModalNavigationStore_CurrentViewModelChanged()
+        {
+            OnPropertyChanged(nameof(CurrentModalViewModel));
+            OnPropertyChanged(nameof(IsModalOpen));
         }
     }
 }
