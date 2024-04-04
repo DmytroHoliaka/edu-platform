51b79e95290a591f492b37ff2bce3f5a914cabcc
HoliakaDmytro <holidim39@gmail.com>
2024-04-04 12:01:13 +0300

    Commands: Add SubmitCreateGroupCommand

---
+++ EduPlatformApp/Commands/SubmitCreateGroupCommand.cs
@@ -0,0 +1,47 @@
+﻿using EduPlatform.WPF.Models;
+using EduPlatform.WPF.Stores;
+using EduPlatform.WPF.ViewModels.GroupsViewModels;
+
+namespace EduPlatform.WPF.Commands
+{
+    public class SubmitCreateGroupCommand : AsyncCommandBase
+    {
+        private readonly GroupStore _groupStore;
+        private readonly CreateGroupViewModel _createGroupViewModel;
+        private readonly ModalNavigationStore _modalNavigationStore;
+
+        public SubmitCreateGroupCommand(GroupStore groupStore,
+                                        CreateGroupViewModel createGroupViewModel,
+                                        ModalNavigationStore modalNavigationStore)
+        {
+            _groupStore = groupStore;
+            _createGroupViewModel = createGroupViewModel;
+            _modalNavigationStore = modalNavigationStore;
+        }
+
+        public override async Task ExecuteAsync(object? parameter)
+        {
+            GroupDetailsFormViewModel formDetails =
+                _createGroupViewModel.GroupDetailsFormVM;
+
+            Group group = new()
+            {
+                GroupId = formDetails.GroupId,
+                Name = formDetails.GroupName,
+                Teachers = formDetails.TeacherVMs
+                    .Where(ft => ft.IsChecked == true)
+                    .Select(ft => ft.Teacher).ToList(),
+                Students = formDetails.StudentVMs
+                    .Where(ft => ft.IsChecked == true)
+                    .Select(fs => fs.Student).ToList()
+            };
+
+            try
+            {
+                await _groupStore.Add(group);
+                _modalNavigationStore.Close();
+            }
+            catch (Exception) { /*ToDo: Make label with error message*/ }
+        }
+    }
+}
