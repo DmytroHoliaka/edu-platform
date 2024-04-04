7961c0bb8987da0eb54d6c60daba8577a23b231f
HoliakaDmytro <holidim39@gmail.com>
2024-04-04 12:03:37 +0300

    Models: Add a model to represent the student

---
+++ EduPlatformApp/Models/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace EduPlatform.WPF.Models
+{
+    public class Student
+    {
+        public Guid StudentId { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+
+        public int GroupId { get; set; }
+        public Group? Group { get; set; }
+    }
+}
