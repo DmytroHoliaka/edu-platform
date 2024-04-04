8e93027ffc684cdad258836bdfa303fc88588297
HoliakaDmytro <holidim39@gmail.com>
2024-04-04 12:03:32 +0300

    Models: Add a model to represent the group

---
+++ EduPlatformApp/Models/Group.cs
@@ -0,0 +1,14 @@
+﻿namespace EduPlatform.WPF.Models
+{
+    public class Group
+    {
+        public Guid GroupId { get; set; }
+        public string? Name { get; set; }
+
+        public int CourseId { get; set; }
+        public Course? Course { get; set; }
+
+        public ICollection<Teacher>? Teachers { get; set; }
+        public ICollection<Student>? Students { get; set; }
+    }
+}
