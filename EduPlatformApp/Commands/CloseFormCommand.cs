705d72aff6012ada3a513c95b83363e6427cc4df
HoliakaDmytro <holidim39@gmail.com>
2024-04-04 12:00:27 +0300

    Commands: Add CloseFormCommand

---
+++ EduPlatformApp/Commands/CloseFormCommand.cs
@@ -0,0 +1,19 @@
+﻿using EduPlatform.WPF.Stores;
+
+namespace EduPlatform.WPF.Commands
+{
+    public class CloseFormCommand : CommandBase
+    {
+        private readonly ModalNavigationStore _modalNavigationStore;
+
+        public CloseFormCommand(ModalNavigationStore modalNavigationStore)
+        {
+            _modalNavigationStore = modalNavigationStore;
+        }
+
+        public override void Execute(object? parameter)
+        {
+            _modalNavigationStore.Close();
+        }
+    }
+}
