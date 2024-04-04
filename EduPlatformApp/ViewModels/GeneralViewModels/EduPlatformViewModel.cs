e989f88a54287477911a35a5fe08731630374c81
HoliakaDmytro <holidim39@gmail.com>
2024-04-04 12:06:01 +0300

    GeneralViewModels: Rename view model properties

--- EduPlatformApp/ViewModels/GeneralViewModels/EduPlatformViewModel.cs
+++ EduPlatformApp/ViewModels/GeneralViewModels/EduPlatformViewModel.cs
@@ -1,34 +1,33 @@
-﻿using EduPlatform.WPF.ViewModels.GeneralViewModels.OverviewViewModel;
-using EduPlatform.WPF.ViewModels.GroupViewModels;
+﻿using EduPlatform.WPF.Stores;
+using EduPlatform.WPF.ViewModels.GeneralViewModels.OverviewViewModel;
+using EduPlatform.WPF.ViewModels.GroupsViewModels;
 using EduPlatform.WPF.ViewModels.NavigationsViewModel;
 using EduPlatform.WPF.ViewModels.StudentsViewModel;
-using EduPlatform.WPF.ViewModels.TeacherViewModel;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Input;
+using EduPlatform.WPF.ViewModels.TeachersViewModel;
 
 namespace EduPlatform.WPF.ViewModels.GeneralViewModels
 {
     public class EduPlatformViewModel : ViewModelBase
     {
-        public NavigationViewModel NavigationViewModel { get; }
-        public EduPlatformOverviewViewModel EduPlatformOverviewViewModel { get; }
-        public EduPlatformGroupsViewModel EduPlatformGroupsViewModel { get; }
-        public EduPlatformStudentsViewModel EduPlatformStudentsViewModel { get; }
-        public EduPlatformTeachersViewModel EduPlatformTeachersViewModel { get; }
-        public GroupDetailsFormViewModel GroupDetailsFormViewModel { get; }
+        public NavigationViewModel NavigationVM { get; }
+        public EduPlatformOverviewViewModel EduPlatformOverviewVM { get; }
+        public GroupSequenceViewModel GroupSequenceVM { get; }
+        public StudentSequenceViewModel StudentSequenceVM { get; }
+        public TeacherSequenceViewModel TeacherSequenceVM { get; }
 
-        public EduPlatformViewModel()
+        public EduPlatformViewModel(GroupStore groupStore, ModalNavigationStore modalNavigationStore)
         {
-            EduPlatformOverviewViewModel = new();
-            EduPlatformGroupsViewModel = new();
-            EduPlatformStudentsViewModel = new();
-            EduPlatformTeachersViewModel = new();
-            NavigationViewModel = new();
-            GroupDetailsFormViewModel = new();
+            EduPlatformOverviewVM = new();
+            StudentSequenceVM = new();
+            TeacherSequenceVM = new();
+            NavigationVM = new();
+            GroupSequenceVM = new
+            (
+                groupStore, 
+                modalNavigationStore, 
+                TeacherSequenceVM,
+                StudentSequenceVM
+            );
         }
     }
 }
