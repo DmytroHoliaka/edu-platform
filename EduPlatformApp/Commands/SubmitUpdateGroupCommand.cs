5fdf232667980fb4f0fcbaa20c684609d9564790
HoliakaDmytro <holidim39@gmail.com>
2024-04-04 12:01:27 +0300

    Commands: Add SubmitUpdateGroupCommand

---
+++ EduPlatformApp/Commands/SubmitUpdateGroupCommand.cs
@@ -0,0 +1,51 @@
+﻿using EduPlatform.WPF.Models;
+using EduPlatform.WPF.Stores;
+using EduPlatform.WPF.ViewModels.GroupsViewModels;
+
+namespace EduPlatform.WPF.Commands
+{
+    public class SubmitUpdateGroupCommand : AsyncCommandBase
+    {
+        private readonly ModalNavigationStore _modalNavigationStore;
+        private readonly GroupStore _groupStore;
+        private readonly GroupViewModel _selectedGroup;
+        private readonly UpdateGroupViewModel _updateGroupViewModel;
+
+        public SubmitUpdateGroupCommand(ModalNavigationStore modalNavigationStore, 
+                                        GroupStore groupStore, 
+                                        GroupViewModel selectedGroup,
+                                        UpdateGroupViewModel updateGroupViewModel)
+        {
+            _modalNavigationStore = modalNavigationStore;
+            _groupStore = groupStore;
+            _selectedGroup = selectedGroup;
+            _updateGroupViewModel = updateGroupViewModel;
+        }
+
+        public override async Task ExecuteAsync(object? parameter)
+        {
+            // ToDo: Add store in database
+            GroupDetailsFormViewModel formDetails = 
+                _updateGroupViewModel.GroupDetailsFormVM;
+
+            Group targetGroup = new()
+            {
+                GroupId = formDetails.GroupId,
+                Name = formDetails.GroupName,
+                Teachers = formDetails.TeacherVMs
+                    .Where(ft => ft.IsChecked == true)
+                    .Select(ft => ft.Teacher).ToList(),
+                Students = formDetails.StudentVMs
+                    .Where(ft => ft.IsChecked == true)
+                    .Select(fs => fs.Student).ToList()
+            };
+
+            try
+            {
+                await _groupStore.Update(_selectedGroup.GroupId, targetGroup);
+                _modalNavigationStore.Close();
+            }
+            catch (Exception) { /*ToDo: Write validation message*/ }
+        }
+    }
+}
