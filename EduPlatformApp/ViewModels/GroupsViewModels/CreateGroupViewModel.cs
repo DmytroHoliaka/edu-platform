b8223665a7a9271324bf5545c6dc3780d604dfa4
HoliakaDmytro <holidim39@gmail.com>
2024-04-04 12:07:13 +0300

    GroupViewModels: Replace CreateGroupViewModel

---
+++ EduPlatformApp/ViewModels/GroupsViewModels/CreateGroupViewModel.cs
@@ -0,0 +1,29 @@
+﻿using EduPlatform.WPF.Commands;
+using EduPlatform.WPF.Stores;
+using EduPlatform.WPF.ViewModels.GeneralViewModels;
+using EduPlatform.WPF.ViewModels.StudentsViewModel;
+using EduPlatform.WPF.ViewModels.TeachersViewModel;
+using System.Windows.Input;
+
+namespace EduPlatform.WPF.ViewModels.GroupsViewModels
+{
+    public class CreateGroupViewModel : ViewModelBase
+    {
+        public GroupDetailsFormViewModel GroupDetailsFormVM { get; }
+
+        public CreateGroupViewModel(GroupStore groupStore,
+                                    ModalNavigationStore modalNavigationStore,
+                                    TeacherSequenceViewModel teacherSequenceVM,
+                                    StudentSequenceViewModel studentSequenceVM)
+        {
+            ICommand? submitCommand = 
+                new SubmitCreateGroupCommand(groupStore, this, modalNavigationStore);
+
+            ICommand? cancelCommand = 
+                new CloseFormCommand(modalNavigationStore);
+
+            GroupDetailsFormVM = 
+                new(Guid.NewGuid(), teacherSequenceVM, studentSequenceVM, submitCommand, cancelCommand);
+        }
+    }
+}
