172ded97281d80dfab8d0c77b719de7f2db62353
HoliakaDmytro <holidim39@gmail.com>
2024-04-04 12:04:58 +0300

    Stores: Add a store with events for working with data

---
+++ EduPlatformApp/Stores/GroupStore.cs
@@ -0,0 +1,20 @@
+﻿using EduPlatform.WPF.Models;
+
+namespace EduPlatform.WPF.Stores
+{
+    public class GroupStore
+    {
+        public event Action<Group>? GroupAdded;
+        public event Action<Guid, Group>? GroupUpdated;
+
+        public async Task Add(Group newGroup)
+        {
+            GroupAdded?.Invoke(newGroup);
+        }
+
+        public async Task Update(Guid sourceId, Group targetGroup)
+        {
+            GroupUpdated?.Invoke(sourceId, targetGroup);
+        }
+    }
+}
