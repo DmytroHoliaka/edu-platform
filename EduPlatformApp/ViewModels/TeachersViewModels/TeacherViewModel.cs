f9d00c8f36c2229102c64f1145920c093ef2ff0f
HoliakaDmytro <holidim39@gmail.com>
2024-04-04 12:10:01 +0300

    GroupViewModels: Replace, rename and implement TeacherViewModel

---
+++ EduPlatformApp/ViewModels/TeachersViewModels/TeacherViewModel.cs
@@ -0,0 +1,59 @@
+﻿using EduPlatform.WPF.Models;
+using EduPlatform.WPF.ViewModels.GeneralViewModels;
+
+namespace EduPlatform.WPF.ViewModels.TeachersViewModels
+{
+    public class TeacherViewModel : ViewModelBase
+    {
+        public Teacher Teacher
+        {
+            get
+            {
+                return _teacher!;
+            }
+            set
+            {
+                _teacher = value;
+                OnPropertyChanged(nameof(Teacher));
+                OnPropertyChanged(nameof(TeacherId));
+                OnPropertyChanged(nameof(FullName));
+                OnPropertyChanged(nameof(IsChecked));
+            }
+        }
+
+        public Guid TeacherId => Teacher.TeacherId;
+
+        public string FullName =>
+            Teacher.FirstName != null || Teacher.LastName != null
+            ? $"{Teacher.FirstName} {Teacher.LastName}"
+            : "Unknown";
+
+        public bool IsChecked
+        {
+            get
+            {
+                return _isChecked;
+            }
+            set
+            {
+                _isChecked = value;
+                OnPropertyChanged(nameof(IsChecked));
+            }
+        }
+
+        private bool _isChecked;
+        private Teacher? _teacher;
+
+        public TeacherViewModel(Teacher teacher)
+        {
+            Teacher = teacher;
+            IsChecked = false;
+        }
+
+        public TeacherViewModel(TeacherViewModel teacherVM)
+        {
+            Teacher = teacherVM.Teacher;
+            IsChecked = false;
+        }
+    }
+}
