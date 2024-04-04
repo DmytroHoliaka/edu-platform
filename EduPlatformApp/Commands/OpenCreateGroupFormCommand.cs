02bc14d91bdc08fa579fd3109f93d290a0713941
HoliakaDmytro <holidim39@gmail.com>
2024-04-04 12:00:37 +0300

    Commands: Add OpenCreateGroupFormCommand

---
+++ EduPlatformApp/Commands/OpenCreateGroupFormCommand.cs
@@ -0,0 +1,34 @@
+﻿using EduPlatform.WPF.Stores;
+using EduPlatform.WPF.ViewModels.GroupsViewModels;
+using EduPlatform.WPF.ViewModels.StudentsViewModel;
+using EduPlatform.WPF.ViewModels.TeachersViewModel;
+
+namespace EduPlatform.WPF.Commands
+{
+    public class OpenCreateGroupFormCommand : CommandBase
+    {
+        private readonly GroupStore _groupStore;
+        private readonly ModalNavigationStore _modalNavigationStore;
+        private readonly TeacherSequenceViewModel _teacherSequenceVM;
+        private readonly StudentSequenceViewModel _studentSequenceVM;
+
+        public OpenCreateGroupFormCommand(GroupStore groupStore,
+                                          ModalNavigationStore modalNavigationStore,
+                                          TeacherSequenceViewModel teacherSequenceVM,
+                                          StudentSequenceViewModel studentSequenceVM)
+        {
+            _groupStore = groupStore;
+            _modalNavigationStore = modalNavigationStore;
+            _teacherSequenceVM = teacherSequenceVM;
+            _studentSequenceVM = studentSequenceVM;
+        }
+
+        public override void Execute(object? parameter)
+        {
+            CreateGroupViewModel createGroupViewModel =
+                new(_groupStore, _modalNavigationStore, _teacherSequenceVM, _studentSequenceVM);
+
+            _modalNavigationStore.CurrentViewModel = createGroupViewModel;
+        }
+    }
+}
