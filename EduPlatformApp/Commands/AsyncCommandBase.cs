75c6d62ca12bc038b8ec9f5229aeca76c5a03f82
HoliakaDmytro <holidim39@gmail.com>
2024-04-04 12:00:57 +0300

    Commands: Add basic class for async commanfs

---
+++ EduPlatformApp/Commands/AsyncCommandBase.cs
@@ -0,0 +1,16 @@
+﻿namespace EduPlatform.WPF.Commands
+{
+    public abstract class AsyncCommandBase : CommandBase
+    {
+        public override async void Execute(object? parameter)
+        {
+            try
+            {
+                await ExecuteAsync(parameter);
+            }
+            catch (Exception) { }
+        }
+
+        public abstract Task ExecuteAsync(object? parameter);
+    }
+}
