206d37993305ccfd099feb647260c669817bef3f
HoliakaDmytro <holidim39@gmail.com>
2024-04-04 12:02:13 +0300

    Components: Add EduPlatformCourses

---
+++ EduPlatformApp/Components/EduPlatformCourses.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace EduPlatform.WPF.Components
+{
+    /// <summary>
+    /// Interaction logic for EduPlatformListing.xaml
+    /// </summary>
+    public partial class EduPlatformCourses : UserControl
+    {
+        public EduPlatformCourses()
+        {
+            InitializeComponent();
+        }
+    }
+}
