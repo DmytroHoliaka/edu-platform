c584c7bbbb930119e0a00e058d2f508a9ba2f2f9
HoliakaDmytro <holidim39@gmail.com>
2024-04-05 13:19:39 +0300

    Components: Rename EduPlatformCourses

--- EduPlatformApp/Components/CourseList.xaml.cs
+++ EduPlatformApp/Components/CourseList.xaml.cs
@@ -5,9 +5,9 @@ namespace EduPlatform.WPF.Components
     /// <summary>
     /// Interaction logic for EduPlatformListing.xaml
     /// </summary>
-    public partial class EduPlatformCourses : UserControl
+    public partial class CourseList : UserControl
     {
-        public EduPlatformCourses()
+        public CourseList()
         {
             InitializeComponent();
         }
