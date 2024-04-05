369e524526e2ed93eea85c36c722f9536fb3c9a9
HoliakaDmytro <holidim39@gmail.com>
2024-04-05 13:26:11 +0300

    ViewModels: Create general view model for creating student

---
+++ EduPlatformApp/ViewModels/StudentsViewModels/CreateStudentViewModel.cs
@@ -0,0 +1,23 @@
+﻿using EduPlatform.WPF.Stores;
+using EduPlatform.WPF.ViewModels.GeneralViewModels;
+using EduPlatform.WPF.ViewModels.GroupsViewModels;
+using System.Windows.Input;
+
+namespace EduPlatform.WPF.ViewModels.StudentsViewModels
+{
+    public class CreateStudentViewModel : ViewModelBase
+    {
+        public StudentDetailsFormViewModel StudentDetailsFormVM { get; }
+
+        public CreateStudentViewModel(GroupStore groupStore,
+                                      ModalNavigationStore modalNavigationStore,
+                                      GroupSequenceViewModel groupSequenceVM)
+        {
+            ICommand? submitCommand = null;
+            ICommand? cancelCommand = null;
+
+            StudentDetailsFormVM =
+                new(Guid.NewGuid(), groupSequenceVM, submitCommand, cancelCommand);
+        }
+    }
+}
