4ac05a564828c15ed07b9f3367492de48565fb5f
HoliakaDmytro <holidim39@gmail.com>
2024-04-05 13:19:02 +0300

    Commands: Add a command to open the form for creating a student.

---
+++ EduPlatformApp/Commands/OpenCreateStudentFormCommand.cs
@@ -0,0 +1,34 @@
+﻿using EduPlatform.WPF.Stores;
+using EduPlatform.WPF.ViewModels.GroupsViewModels;
+using EduPlatform.WPF.ViewModels.StudentsViewModels;
+
+namespace EduPlatform.WPF.Commands
+{
+    public class OpenCreateStudentFormCommand : CommandBase
+    {
+        private readonly GroupStore _groupStore;
+        private readonly ViewStore _viewStore;
+        private readonly ModalNavigationStore _modalNavigationStore;
+        private readonly GroupSequenceViewModel _groupSequenceVM;
+
+        public OpenCreateStudentFormCommand(GroupStore groupStore,
+                                            ViewStore viewStore,
+                                            ModalNavigationStore modalNavigationStore,
+                                            GroupSequenceViewModel groupSequenceVM)
+        {
+            _groupStore = groupStore;
+            _viewStore = viewStore;
+            _modalNavigationStore = modalNavigationStore;
+            _groupSequenceVM = groupSequenceVM;
+        }
+
+        public override void Execute(object? parameter)
+        {
+            CreateStudentViewModel createStudentVM =
+                new(_groupStore, _modalNavigationStore, _groupSequenceVM);
+
+            _modalNavigationStore.CurrentViewModel = createStudentVM;
+            _viewStore.UnfocuseGroup();
+        }
+    }
+}
