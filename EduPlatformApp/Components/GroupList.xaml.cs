09f8a97c166c4b14f24f46c028248fae2c79b5de
HoliakaDmytro <holidim39@gmail.com>
2024-04-05 13:20:07 +0300

    Components: Rename EduPlatformGroups

---
+++ EduPlatformApp/Components/GroupList.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace EduPlatform.WPF.Components
+{
+    /// <summary>
+    /// Interaction logic for EduPlatformGroups.xaml
+    /// </summary>
+    public partial class GroupList : UserControl
+    {
+        public GroupList()
+        {
+            InitializeComponent();
+        }
+    }
+}
