5236c3a1b571a7585cc22d1abc71b0931dd30332
HoliakaDmytro <holidim39@gmail.com>
2024-04-05 14:19:25 +0300

    Stores: Add StudentStore for main data operation with Student model

---
+++ EduPlatformApp/Stores/StudentStore.cs
@@ -0,0 +1,26 @@
+﻿using EduPlatform.WPF.Models;
+
+namespace EduPlatform.WPF.Stores
+{
+    public class StudentStore
+    {
+        public event Action<Student>? StudentAdded;
+        public event Action<Guid, Student>? StudentUpdated;
+        public event Action<Guid>? StudentDeleted;
+
+        public async Task Add(Student newStudent)
+        {
+            StudentAdded?.Invoke(newStudent);
+        }
+
+        public async Task Update(Guid sourceId, Student targetStudent)
+        {
+            StudentUpdated?.Invoke(sourceId, targetStudent);
+        }
+
+        public async Task Delete(Guid studentId)
+        {
+            StudentDeleted?.Invoke(studentId);
+        }
+    }
+}
