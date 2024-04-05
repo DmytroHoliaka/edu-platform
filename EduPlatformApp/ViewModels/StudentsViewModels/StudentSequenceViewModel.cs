dbb8e75c7d7f0d5c209aab6200d8243bb8f32a53
HoliakaDmytro <holidim39@gmail.com>
2024-04-05 13:25:45 +0300

    ViewModels: Implement a class that represents a sequence of student view models

--- EduPlatformApp/ViewModels/StudentsViewModels/StudentSequenceViewModel.cs
+++ EduPlatformApp/ViewModels/StudentsViewModels/StudentSequenceViewModel.cs
@@ -1,7 +1,13 @@
-﻿using EduPlatform.WPF.Models;
+﻿using EduPlatform.WPF.Commands;
+using EduPlatform.WPF.Models;
+using EduPlatform.WPF.Stores;
 using EduPlatform.WPF.ViewModels.GeneralViewModels;
+using EduPlatform.WPF.ViewModels.GroupsViewModels;
 using EduPlatform.WPF.ViewModels.StudentsViewModels;
+using EduPlatform.WPF.ViewModels.TeachersViewModel;
 using System.Collections.ObjectModel;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace EduPlatform.WPF.ViewModels.StudentsViewModel
 {
@@ -11,9 +17,28 @@ namespace EduPlatform.WPF.ViewModels.StudentsViewModel
         public IEnumerable<StudentViewModel> StudentVMs =>
             _studentVMs.Select(s => new StudentViewModel(s));
 
-        public StudentSequenceViewModel()
+        public StudentViewModel? SelectedStudent { get; set; }
+
+        public ICommand CreateGroupCommand { get; private set; }
+        public ICommand UpdateGroupCommand { get; private set; }
+        public ICommand DeleteGroupCommand { get; private set; }
+
+        private GroupSequenceViewModel? _groupSequenceVM;
+        private readonly GroupStore _groupStore;
+        private readonly ViewStore _viewStore;
+        private readonly ModalNavigationStore _modalNavigationStore;
+
+        public StudentSequenceViewModel
+        (
+            GroupStore groupStore,
+            ViewStore viewStore,
+            ModalNavigationStore modalNavigationStore
+        )
         {
             _studentVMs = [];
+            _groupStore = groupStore;
+            _viewStore = viewStore;
+            _modalNavigationStore = modalNavigationStore;
 
             Student student1 = new()
             {
@@ -29,32 +54,62 @@ namespace EduPlatform.WPF.ViewModels.StudentsViewModel
                 LastName = "Student",
             };
 
-            Student student3 = new()
-            {
-                StudentId = Guid.NewGuid(),
-                FirstName = "Rick",
-                LastName = "Student",
-            };
-
-            Student student4 = new()
-            {
-                StudentId = Guid.NewGuid(),
-                FirstName = "Thomas",
-                LastName = "Student",
-            };
-
-            Student student5 = new()
-            {
-                StudentId = Guid.NewGuid(),
-                FirstName = "Richard",
-                LastName = "Student",
-            };
-
             _studentVMs.Add(new StudentViewModel(student1));
             _studentVMs.Add(new StudentViewModel(student2));
-            _studentVMs.Add(new StudentViewModel(student3));
-            _studentVMs.Add(new StudentViewModel(student4));
-            _studentVMs.Add(new StudentViewModel(student5));
+        }
+
+        public void SetGroupSequence(GroupSequenceViewModel newGroup)
+        {
+            _groupSequenceVM = newGroup;
+        }
+
+        public void ConfigureCommands()
+        {
+            CreateGroupCommand = new OpenCreateStudentFormCommand(_groupStore, _viewStore, _modalNavigationStore, _groupSequenceVM);
+            UpdateGroupCommand = null;
+            DeleteGroupCommand = null;
         }
     }
 }
+
+
+//Student student1 = new()
+//{
+//    StudentId = Guid.NewGuid(),
+//    FirstName = "John",
+//    LastName = "Student",
+//};
+
+//Student student2 = new()
+//{
+//    StudentId = Guid.NewGuid(),
+//    FirstName = "Alex",
+//    LastName = "Student",
+//};
+
+//Student student3 = new()
+//{
+//    StudentId = Guid.NewGuid(),
+//    FirstName = "Rick",
+//    LastName = "Student",
+//};
+
+//Student student4 = new()
+//{
+//    StudentId = Guid.NewGuid(),
+//    FirstName = "Thomas",
+//    LastName = "Student",
+//};
+
+//Student student5 = new()
+//{
+//    StudentId = Guid.NewGuid(),
+//    FirstName = "Richard",
+//    LastName = "Student",
+//};
+
+//_studentVMs.Add(new StudentViewModel(student1));
+//_studentVMs.Add(new StudentViewModel(student2));
+//_studentVMs.Add(new StudentViewModel(student3));
+//_studentVMs.Add(new StudentViewModel(student4));
+//_studentVMs.Add(new StudentViewModel(student5));
