2404d816a81e6741f978b0d284291f1f40ee0f11
HoliakaDmytro <holidim39@gmail.com>
2024-04-05 14:20:08 +0300

    ViewStore: Add StudentUnfocused event

--- EduPlatformApp/Stores/ViewStore.cs
+++ EduPlatformApp/Stores/ViewStore.cs
@@ -3,10 +3,16 @@
     public class ViewStore
     {
         public event Action? GroupUnfocused;
+        public event Action? StudentUnfocused;
 
         public void UnfocuseGroup()
         {
             GroupUnfocused?.Invoke();
+        }
+
+        public void UnfocuseStudent()
+        {
+            StudentUnfocused?.Invoke();
         }
     }
 }
