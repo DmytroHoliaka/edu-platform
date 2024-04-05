92d1da730cfd009541242ece363777f266794f10
HoliakaDmytro <holidim39@gmail.com>
2024-04-05 13:20:28 +0300

    Components: Rename EduPlatformStudents

---
+++ EduPlatformApp/Components/StudentList.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace EduPlatform.WPF.Components
+{
+    /// <summary>
+    /// Interaction logic for EduPlatformStudents.xaml
+    /// </summary>
+    public partial class StudentList : UserControl
+    {
+        public StudentList()
+        {
+            InitializeComponent();
+        }
+    }
+}
