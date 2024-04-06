868723316634caa4dfe4d74b16b880ba30508d71
HoliakaDmytro <holidim39@gmail.com>
2024-04-06 11:55:01 +0300

    Commands: Add command to open the student update form

---
+++ EduPlatformApp/Commands/OpenUpdateStudentFormCommand.cs
@@ -0,0 +1,45 @@
+﻿using EduPlatform.WPF.Stores;
+using EduPlatform.WPF.ViewModels.GroupsViewModels;
+using EduPlatform.WPF.ViewModels.StudentsViewModels;
+using System.Windows.Media.Animation;
+
+namespace EduPlatform.WPF.Commands
+{
+    public class OpenUpdateStudentFormCommand : CommandBase
+    {
+        public StudentViewModel? UpdatingStudent { get; set; }
+
+        private readonly StudentStore _studentStore;
+        private readonly ViewStore _viewStore;
+        private readonly ModalNavigationStore _modalNavigationStore;
+        private readonly GroupSequenceViewModel _groupSequenceVM;
+
+
+        public OpenUpdateStudentFormCommand(StudentStore studentStore,
+                                            StudentViewModel? selectedStudent,
+                                            ViewStore viewStore,
+                                            ModalNavigationStore modalNavigationStore,
+                                            GroupSequenceViewModel groupSequenceVM)
+        {
+            _studentStore = studentStore;
+            UpdatingStudent = selectedStudent;
+            _viewStore = viewStore;
+            _modalNavigationStore = modalNavigationStore;
+            _groupSequenceVM = groupSequenceVM;
+        }
+
+        public override bool CanExecute(object? parameter)
+        {
+            return UpdatingStudent is not null;
+        }
+
+        public override void Execute(object? parameter)
+        {
+            UpdateStudentViewModel createStudentVM =
+                new(_studentStore, UpdatingStudent!, _viewStore, _modalNavigationStore, _groupSequenceVM);
+
+            _modalNavigationStore.CurrentViewModel = createStudentVM;
+            _viewStore.UnfocuseStudent();
+        }
+    }
+}
