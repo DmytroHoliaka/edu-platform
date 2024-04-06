6f51730cadb30e8517ad39cc8282bdbb4e3df8b5
HoliakaDmytro <holidim39@gmail.com>
2024-04-06 12:32:14 +0300

    Commands: Add command to delete a student

---
+++ EduPlatformApp/Commands/DeleteStudentCommand.cs
@@ -0,0 +1,43 @@
+﻿using EduPlatform.WPF.Stores;
+using EduPlatform.WPF.ViewModels.StudentsViewModels;
+
+namespace EduPlatform.WPF.Commands
+{
+    public class DeleteStudentCommand : AsyncCommandBase
+    {
+        public StudentViewModel? DeletingStudent { get; set; }
+
+        private readonly StudentStore _studentStore;
+        private readonly ModalNavigationStore _modalNavigationStore;
+
+        public DeleteStudentCommand(StudentStore studentStore,
+                                    ModalNavigationStore modalNavigationStore)
+        {
+            _studentStore = studentStore;
+            _modalNavigationStore = modalNavigationStore;
+        }
+
+        public override bool CanExecute(object? parameter)
+        {
+            return DeletingStudent is not null;
+        }
+
+        public override async Task ExecuteAsync(object? parameter)
+        {
+            // ToDo: Add store in database
+            if (DeletingStudent is null)
+            {
+                return;
+            }
+
+            Guid studentId = DeletingStudent.StudentId;
+
+            try
+            {
+                await _studentStore.Delete(studentId);
+                _modalNavigationStore.Close();
+            }
+            catch (Exception) { /*ToDo: Write validation message*/ }
+        }
+    }
+}
