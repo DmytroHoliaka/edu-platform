86df862ebae1d619d79cae04ee13e3a82f7500a4
HoliakaDmytro <holidim39@gmail.com>
2024-04-07 13:14:39 +0300

    Components: Configure property PreviewMouseDown in StudentDetailForm

--- EduPlatformApp/Components/StudentDetailForm.xaml.cs
+++ EduPlatformApp/Components/StudentDetailForm.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace EduPlatform.WPF.Components
 {
@@ -23,6 +24,21 @@ namespace EduPlatform.WPF.Components
         public StudentDetailForm()
         {
             InitializeComponent();
+        }
+
+        private void RadioButton_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            RadioButton radioButton = (RadioButton)sender;
+
+            if (radioButton.IsChecked == true)
+            {
+                Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    radioButton.IsChecked = false;
+                }), DispatcherPriority.Input);
+
+                e.Handled = true;
+            }
         }
     }
 }
