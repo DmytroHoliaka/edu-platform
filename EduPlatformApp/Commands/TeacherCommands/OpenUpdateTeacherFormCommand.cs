589cca208abe8962f924d6083ab17dd46cd33b5f
HoliakaDmytro <holidim39@gmail.com>
2024-04-11 12:07:00 +0300

    Commands: Adjust OpenUpdateTeacherFormCommand to work with teacher

--- EduPlatformApp/Commands/TeacherCommands/OpenUpdateTeacherFormCommand.cs
+++ EduPlatformApp/Commands/TeacherCommands/OpenUpdateTeacherFormCommand.cs
@@ -6,24 +6,24 @@ using EduPlatform.WPF.ViewModels.TeachersViewModels;
 
 namespace EduPlatform.WPF.Commands.TeacherCommands
 {
-    public class OpenUpdateTeacherFormCommand: CommandBase
+    public class OpenUpdateTeacherFormCommand : CommandBase
     {
-        public TeacherViewModel? UpdatingStudent { get; set; }
+        public TeacherViewModel? UpdatingTeacher { get; set; }
 
-        private readonly StudentStore _studentStore;
+        private readonly TeacherStore _teacherStore;
         private readonly ViewStore _viewStore;
         private readonly ModalNavigationStore _modalNavigationStore;
         private readonly GroupSequenceViewModel _groupSequenceVM;
 
 
-        public OpenUpdateTeacherFormCommand(StudentStore studentStore,
-                                            StudentViewModel? selectedStudent,
+        public OpenUpdateTeacherFormCommand(TeacherStore teacherStore,
+                                            TeacherViewModel? selectedTeacher,
                                             ViewStore viewStore,
                                             ModalNavigationStore modalNavigationStore,
                                             GroupSequenceViewModel groupSequenceVM)
         {
-            _studentStore = studentStore;
-            UpdatingStudent = selectedStudent;
+            _teacherStore = teacherStore;
+            UpdatingTeacher = selectedTeacher;
             _viewStore = viewStore;
             _modalNavigationStore = modalNavigationStore;
             _groupSequenceVM = groupSequenceVM;
@@ -31,15 +31,15 @@ namespace EduPlatform.WPF.Commands.TeacherCommands
 
         public override bool CanExecute(object? parameter)
         {
-            return UpdatingStudent is not null;
+            return UpdatingTeacher is not null;
         }
 
         public override void Execute(object? parameter)
         {
-            UpdateStudentViewModel createStudentVM =
-                new(_studentStore, UpdatingStudent!, _viewStore, _modalNavigationStore, _groupSequenceVM);
+            UpdateTeacherViewModel createTeacherVM =
+                new(_teacherStore, UpdatingTeacher!, _viewStore, _modalNavigationStore, _groupSequenceVM);
 
-            _modalNavigationStore.CurrentViewModel = createStudentVM;
+            _modalNavigationStore.CurrentViewModel = createTeacherVM;
             _viewStore.UnfocuseStudent();
         }
     }
