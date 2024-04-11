2c8f7c9a7e39ba1b1767e00fb2ed0842add7a3be
HoliakaDmytro <holidim39@gmail.com>
2024-04-11 10:03:04 +0300

    ViewModels: Implement logic for TeacherSequenceViewModel

--- EduPlatformApp/ViewModels/TeachersViewModels/TeacherSequenceViewModel.cs
+++ EduPlatformApp/ViewModels/TeachersViewModels/TeacherSequenceViewModel.cs
@@ -1,7 +1,14 @@
-﻿using EduPlatform.WPF.Models;
+﻿using EduPlatform.WPF.Commands.StudentCommands;
+using EduPlatform.WPF.Commands.TeacherCommands;
+using EduPlatform.WPF.Models;
+using EduPlatform.WPF.Stores;
 using EduPlatform.WPF.ViewModels.GeneralViewModels;
+using EduPlatform.WPF.ViewModels.GroupsViewModels;
+using EduPlatform.WPF.ViewModels.StudentsViewModels;
 using EduPlatform.WPF.ViewModels.TeachersViewModels;
 using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Input;
 
 namespace EduPlatform.WPF.ViewModels.TeachersViewModel
 {
@@ -11,11 +18,73 @@ namespace EduPlatform.WPF.ViewModels.TeachersViewModel
         public IEnumerable<TeacherViewModel> TeacherVMs =>
             _teacherVMs.Select(tvm => new TeacherViewModel(tvm.Teacher));
 
+        public TeacherViewModel? SelectedTeacher
+        {
+            get
+            {
+                return _selectedTeacher;
+            }
+            set
+            {
+                _selectedTeacher = value;
+                OnPropertyChanged(nameof(SelectedTeacher));
+            }
+        }
 
-        public TeacherSequenceViewModel()
+        public ICommand CreateTeacherCommand { get; private set; }
+        public ICommand UpdateTeacherCommand { get; private set; }
+        public ICommand DeleteTeacherCommand { get; private set; }
+
+        private GroupSequenceViewModel? _groupSequenceVM;
+        private TeacherViewModel? _selectedTeacher;
+        private readonly TeacherStore _teacherStore;
+        private readonly ViewStore _viewStore;
+        private readonly ModalNavigationStore _modalNavigationStore;
+
+
+        public TeacherSequenceViewModel
+        (
+            TeacherStore teacherStore,
+            ViewStore viewStore,
+            ModalNavigationStore modalNavigationStore
+        )
         {
             _teacherVMs = [];
+            _teacherStore = teacherStore;
+            _teacherStore.TeacherAdded += TeacherStore_TeacherAdded;
+            _teacherStore.TeacherUpdated += TeacherStore_TeacherUpdated;
+            _teacherStore.TeacherDeleted += TeacherStore_TeacherDeleted;
 
+            _viewStore = viewStore;
+            _viewStore.TeacherUnfocused += ViewStore_TeacherUnfocused;
+
+            _modalNavigationStore = modalNavigationStore;
+        }
+
+        public void SetGroupSequence(GroupSequenceViewModel newGroup)
+        {
+            _groupSequenceVM = newGroup;
+        }
+
+        public void ConfigureCommands()
+        {
+            if (_groupSequenceVM is null)
+            {
+                return;
+            }
+
+            //CreateStudentCommand = new OpenCreateStudentFormCommand(_studentStore, _viewStore, _modalNavigationStore, _groupSequenceVM);
+            //UpdateStudentCommand = new OpenUpdateStudentFormCommand(_studentStore, _selectedStudent, _viewStore, _modalNavigationStore, _groupSequenceVM);
+            //DeleteStudentCommand = new DeleteStudentCommand(_studentStore, _modalNavigationStore);
+
+            CreateTeacherCommand = new OpenCreateTeacherFormCommand(_teacherStore, _viewStore, _modalNavigationStore, _groupSequenceVM);
+            UpdateTeacherCommand = null;
+            DeleteTeacherCommand = null;
+        }
+
+        // ToDo: Remove
+        public void InsertTestData()
+        {
             Teacher teacher1 = new()
             {
                 TeacherId = Guid.NewGuid(),
@@ -56,6 +125,49 @@ namespace EduPlatform.WPF.ViewModels.TeachersViewModel
             _teacherVMs.Add(new TeacherViewModel(teacher3));
             _teacherVMs.Add(new TeacherViewModel(teacher4));
             _teacherVMs.Add(new TeacherViewModel(teacher5));
+        }
+
+        private void AddTeacher(Teacher teacher)
+        {
+            TeacherViewModel teacherVM = new(teacher);
+            _teacherVMs.Add(teacherVM);
+        }
+
+        private void UpdateTeacher()
+        {
+            MessageBox.Show("UpdateTeacher button was pressed");
+        }
+
+        private void DeleteTeacher()
+        {
+            MessageBox.Show("DeleteTeacher button was pressed");
+        }
+
+        private void UnfocuseTeacher()
+        {
+            MessageBox.Show("UnfocuseTeacher button was pressed");
+        }
+
+
+        private void TeacherStore_TeacherAdded(Teacher teacher)
+        {
+            AddTeacher(teacher);
+            OnPropertyChanged(nameof(TeacherVMs));
+        }
+
+        private void TeacherStore_TeacherDeleted(Guid obj)
+        {
+            UpdateTeacher();
+        }
+
+        private void TeacherStore_TeacherUpdated(Teacher obj)
+        {
+            DeleteTeacher();
+        }
+
+        private void ViewStore_TeacherUnfocused()
+        {
+            UnfocuseTeacher();
         }
     }
 }
