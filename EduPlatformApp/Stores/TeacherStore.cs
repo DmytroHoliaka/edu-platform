2a277d82c3d48c32888d26dea4cce7483777ad85
HoliakaDmytro <holidim39@gmail.com>
2024-04-11 09:57:05 +0300

    Stores: Add a store for teachers

---
+++ EduPlatformApp/Stores/TeacherStore.cs
@@ -0,0 +1,26 @@
+﻿using EduPlatform.WPF.Models;
+
+namespace EduPlatform.WPF.Stores
+{
+    public class TeacherStore
+    {
+        public event Action<Teacher>? TeacherAdded;
+        public event Action<Teacher>? TeacherUpdated;
+        public event Action<Guid>? TeacherDeleted;
+
+        public async Task Add(Teacher newTeacher)
+        {
+            TeacherAdded?.Invoke(newTeacher);
+        }
+
+        public async Task Update(Teacher targetTeacher)
+        {
+            TeacherUpdated?.Invoke(targetTeacher);
+        }
+
+        public async Task Delete(Guid teacherId)
+        {
+            TeacherDeleted?.Invoke(teacherId);
+        }
+    }
+}
