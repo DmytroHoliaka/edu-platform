099f5383c28fc78a7d2ddb40122948a35c3ce137
HoliakaDmytro <holidim39@gmail.com>
2024-04-13 16:37:43 +0300

    ViewModels: Add CourseViewModel

---
+++ EduPlatformApp/ViewModels/CoursesViewModels/CourseViewModel.cs
@@ -0,0 +1,57 @@
+﻿using EduPlatform.WPF.Models;
+using EduPlatform.WPF.ViewModels.GeneralViewModels;
+
+namespace EduPlatform.WPF.ViewModels.TeachersViewModels
+{
+    public class CourseViewModel : ViewModelBase
+    {
+        public Course Course
+        {
+            get
+            {
+                return _course!;
+            }
+            set
+            {
+                _course = value;
+                OnPropertyChanged(nameof(Course));
+                OnPropertyChanged(nameof(CourseId));
+                OnPropertyChanged(nameof(Groups));
+                OnPropertyChanged(nameof(CourseName));
+                OnPropertyChanged(nameof(IsChecked));
+            }
+        }
+
+        public Guid CourseId => 
+            Course.CourseId;
+
+        public IEnumerable<Group> Groups =>
+            Course.Groups ?? Enumerable.Empty<Group>();
+
+        public string CourseName =>
+            Course?.Name ?? "unknown";
+
+
+        public bool IsChecked
+        {
+            get
+            {
+                return _isChecked;
+            }
+            set
+            {
+                _isChecked = value;
+                OnPropertyChanged(nameof(IsChecked));
+            }
+        }
+
+        private bool _isChecked;
+        private Course? _course;
+
+        public CourseViewModel(Course course)
+        {
+            Course = course;
+            IsChecked = false;
+        }
+    }
+}
