f7c5629541dfef6ea988cdc0016db8af9d7cd81d
HoliakaDmytro <holidim39@gmail.com>
2024-04-13 16:35:48 +0300

    Stores: Add CourseStore for main data operation with course model

---
+++ EduPlatformApp/Stores/CourseStore.cs
@@ -0,0 +1,26 @@
+﻿using EduPlatform.WPF.Models;
+
+namespace EduPlatform.WPF.Stores
+{
+    public class CourseStore
+    {
+        public event Action<Course>? CourseAdded;
+        public event Action<Course>? CourseUpdated;
+        public event Action<Guid>? CourseDeleted;
+
+        public async Task Add(Course newCourse)
+        {
+            CourseAdded?.Invoke(newCourse);
+        }
+
+        public async Task Update(Course targetCourse)
+        {
+            CourseUpdated?.Invoke(targetCourse);
+        }
+
+        public async Task Delete(Guid courseId)
+        {
+            CourseDeleted?.Invoke(courseId);
+        }
+    }
+}
