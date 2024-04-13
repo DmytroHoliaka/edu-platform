55fc8307ee1c7fcf8036aa2b01430e56c1c856ff
HoliakaDmytro <holidim39@gmail.com>
2024-04-13 16:37:25 +0300

    ViewModels: Add CourseDetailsFormViewModel

---
+++ EduPlatformApp/ViewModels/CoursesViewModels/CourseDetailsFormViewModel.cs
@@ -0,0 +1,49 @@
+﻿using EduPlatform.WPF.ViewModels.GeneralViewModels;
+using EduPlatform.WPF.ViewModels.GroupsViewModels;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace EduPlatform.WPF.ViewModels.TeachersViewModels
+{
+    public class CourseDetailsFormViewModel : ViewModelBase
+    {
+        public ObservableCollection<GroupViewModel> GroupVMs { get; }
+
+        public string? CourseName
+        {
+            get
+            {
+                return _courseName;
+            }
+            set
+            {
+                _courseName = value;
+                OnPropertyChanged(nameof(CourseName));
+                OnPropertyChanged(nameof(CanSubmit));
+            }
+        }
+
+        public bool CanSubmit =>
+            string.IsNullOrWhiteSpace(CourseName) == false;
+
+        public ICommand SubmitCommand { get; }
+        public ICommand CancelCommand { get; }
+
+        private string? _courseName;
+
+
+        public CourseDetailsFormViewModel
+        (
+            GroupSequenceViewModel groupSequenceVM,
+            ICommand submitCommand,
+            ICommand cancelCommand
+        )
+        {
+            GroupVMs = new(groupSequenceVM.GroupVMs);
+
+            SubmitCommand = submitCommand;
+            CancelCommand = cancelCommand;
+        }
+    }
+}
