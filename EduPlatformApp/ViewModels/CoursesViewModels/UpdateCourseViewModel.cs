87849ef5f50596c969d6a985b45ef4acd0d8f55c
HoliakaDmytro <holidim39@gmail.com>
2024-04-13 16:37:59 +0300

    ViewModels: Add UpdateCourseViewModel

---
+++ EduPlatformApp/ViewModels/CoursesViewModels/UpdateCourseViewModel.cs
@@ -0,0 +1,42 @@
+﻿using EduPlatform.WPF.Commands.GeneralCommands;
+using EduPlatform.WPF.Commands.StudentCommands;
+using EduPlatform.WPF.Commands.TeacherCommands;
+using EduPlatform.WPF.Stores;
+using EduPlatform.WPF.ViewModels.GeneralViewModels;
+using EduPlatform.WPF.ViewModels.GroupsViewModels;
+using EduPlatform.WPF.ViewModels.StudentsViewModels;
+using System.Windows.Input;
+
+namespace EduPlatform.WPF.ViewModels.TeachersViewModels
+{
+    public class UpdateCourseViewModel : ViewModelBase
+    {
+        public CourseDetailsFormViewModel CourseDetailsFormVM { get; }
+
+        public UpdateCourseViewModel(CourseStore courseStore,
+                                     CourseViewModel selectedCourse,
+                                     ViewStore viewStore,
+                                     ModalNavigationStore modalNavigationStore,
+                                     GroupSequenceViewModel groupSequenceVM)
+        {
+            //ICommand submitCommand = new SubmitUpdateTeacherCommand(selectedCourse, modalNavigationStore, courseStore);
+            //ICommand cancelCommand = new CloseFormCommand(modalNavigationStore);
+
+            ICommand submitCommand = null;
+            ICommand cancelCommand = null;
+
+            CourseDetailsFormVM = new(groupSequenceVM, submitCommand, cancelCommand)
+            {
+                CourseName = selectedCourse.CourseName
+            };
+
+            CourseDetailsFormVM.GroupVMs
+                .Where(gvm => selectedCourse.Groups.Any(g => g.GroupId == gvm.GroupId))
+                .ToList()
+                .ForEach(gvm => gvm.IsChecked = true);
+
+            //((SubmitUpdateTeacherCommand)submitCommand).FormDetails = CourseDetailsFormVM;
+            viewStore.UnfocuseCourse();
+        }
+    }
+}
