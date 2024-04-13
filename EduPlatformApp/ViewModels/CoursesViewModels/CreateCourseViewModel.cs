3a50ade769a0b4ae436a84329a2105aff63807c3
HoliakaDmytro <holidim39@gmail.com>
2024-04-13 16:37:51 +0300

    ViewModels: Add CreateCourseViewModel

---
+++ EduPlatformApp/ViewModels/CoursesViewModels/CreateCourseViewModel.cs
@@ -0,0 +1,32 @@
+﻿using EduPlatform.WPF.Commands.GeneralCommands;
+using EduPlatform.WPF.Commands.StudentCommands;
+using EduPlatform.WPF.Commands.TeacherCommands;
+using EduPlatform.WPF.Stores;
+using EduPlatform.WPF.ViewModels.GeneralViewModels;
+using EduPlatform.WPF.ViewModels.GroupsViewModels;
+using EduPlatform.WPF.ViewModels.StudentsViewModels;
+using System.Windows.Input;
+
+namespace EduPlatform.WPF.ViewModels.TeachersViewModels
+{
+    public class CreateCourseViewModel : ViewModelBase
+    {
+        public CourseDetailsFormViewModel CourseDetailsFormVM { get; }
+
+        public CreateCourseViewModel(CourseStore courseStore,
+                                     ModalNavigationStore modalNavigationStore,
+                                     GroupSequenceViewModel groupSequenceVM)
+        {
+            //ICommand submitCommand = new SubmitCreateTeacherCommand(courseStore, modalNavigationStore);
+            //ICommand cancelCommand = new CloseFormCommand(modalNavigationStore);
+
+            ICommand submitCommand = null;
+            ICommand cancelCommand = null;
+
+            CourseDetailsFormVM =
+                new(groupSequenceVM, submitCommand, cancelCommand);
+
+            //((SubmitCreateTeacherCommand)submitCommand).FormDetails = CourseDetailsFormVM;
+        }
+    }
+}
