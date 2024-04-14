76906f28d59ef43d0a82e008428570367289a953
HoliakaDmytro <holidim39@gmail.com>
2024-04-14 14:17:08 +0300

    CustomControls: Add CustomContentControl with ability to control change event

---
+++ EduPlatformApp/CustomControls/CustomContentControl.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+
+namespace EduPlatform.WPF.CustomControls
+{
+    public class CustomContentControl : ContentControl
+    {
+        public static readonly RoutedEvent ContentChangedEvent =
+            EventManager.RegisterRoutedEvent("ContentChanged", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(CustomContentControl));
+
+        public event RoutedEventHandler ContentChanged
+        {
+            add { AddHandler(ContentChangedEvent, value); }
+            remove { RemoveHandler(ContentChangedEvent, value); }
+        }
+
+        protected override void OnContentChanged(object oldContent, object newContent)
+        {
+            base.OnContentChanged(oldContent, newContent);
+
+            RoutedEventArgs args = new(ContentChangedEvent);
+            RaiseEvent(args);
+        }
+    }
+
+}
