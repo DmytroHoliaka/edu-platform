295c23c500b47c8669bc772579e3981e37fb4ffb
HoliakaDmytro <holidim39@gmail.com>
2024-04-14 14:14:34 +0300

    Commands: Add CourseSequenceViewModel to OpenCreateGroupFormCommand

--- EduPlatformApp/Commands/GroupCommands/OpenCreateGroupFormCommand.cs
+++ EduPlatformApp/Commands/GroupCommands/OpenCreateGroupFormCommand.cs
@@ -1,5 +1,6 @@
 ﻿using EduPlatform.WPF.Commands.BaseCommands;
 using EduPlatform.WPF.Stores;
+using EduPlatform.WPF.ViewModels.CoursesViewModels;
 using EduPlatform.WPF.ViewModels.GroupsViewModels;
 using EduPlatform.WPF.ViewModels.StudentsViewModel;
 using EduPlatform.WPF.ViewModels.TeachersViewModel;
@@ -11,26 +12,32 @@ namespace EduPlatform.WPF.Commands.GroupCommands
         private readonly GroupStore _groupStore;
         private readonly ViewStore _viewStore;
         private readonly ModalNavigationStore _modalNavigationStore;
-        private readonly TeacherSequenceViewModel _teacherSequenceVM;
-        private readonly StudentSequenceViewModel _studentSequenceVM;
+        private readonly CourseSequenceViewModel? _courseSequenceVM;
+        private readonly TeacherSequenceViewModel? _teacherSequenceVM;
+        private readonly StudentSequenceViewModel? _studentSequenceVM;
 
         public OpenCreateGroupFormCommand(GroupStore groupStore,
                                           ViewStore viewStore,
                                           ModalNavigationStore modalNavigationStore,
-                                          TeacherSequenceViewModel teacherSequenceVM,
-                                          StudentSequenceViewModel studentSequenceVM)
+                                          CourseSequenceViewModel? courseSequenceVM,
+                                          TeacherSequenceViewModel? teacherSequenceVM,
+                                          StudentSequenceViewModel? studentSequenceVM)
         {
             _groupStore = groupStore;
             _viewStore = viewStore;
             _modalNavigationStore = modalNavigationStore;
+            _courseSequenceVM = courseSequenceVM;
             _teacherSequenceVM = teacherSequenceVM;
             _studentSequenceVM = studentSequenceVM;
         }
 
         public override void Execute(object? parameter)
         {
-            CreateGroupViewModel createGroupViewModel =
-                new(_groupStore, _modalNavigationStore, _teacherSequenceVM, _studentSequenceVM);
+            CreateGroupViewModel createGroupViewModel = new(_groupStore,
+                                                            _modalNavigationStore,
+                                                            _courseSequenceVM,
+                                                            _teacherSequenceVM,
+                                                            _studentSequenceVM);
 
             _modalNavigationStore.CurrentViewModel = createGroupViewModel;
             _viewStore.UnfocuseGroup();
