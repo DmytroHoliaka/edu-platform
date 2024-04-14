ebee7198bb635bc2109e05fe750a64bf752bf5f5
HoliakaDmytro <holidim39@gmail.com>
2024-04-14 11:38:52 +0300

    Commands: Add DeleteCourseCommand

---
+++ EduPlatformApp/Commands/CourseCommands/DeleteCourseCommand.cs
@@ -0,0 +1,43 @@
+﻿using EduPlatform.WPF.Commands.BaseCommands;
+using EduPlatform.WPF.Stores;
+using EduPlatform.WPF.ViewModels.CoursesViewModels;
+
+namespace EduPlatform.WPF.Commands.TeacherCommands
+{
+    public class DeleteCourseCommand : AsyncCommandBase
+    {
+        public CourseViewModel? DeletingCourse { get; set; }
+
+        private readonly CourseStore _courseStore;
+        private readonly ModalNavigationStore _modalNavigationStore;
+
+        public DeleteCourseCommand(CourseStore courseStore,
+                                    ModalNavigationStore modalNavigationStore)
+        {
+            _courseStore = courseStore;
+            _modalNavigationStore = modalNavigationStore;
+        }
+
+        public override bool CanExecute(object? parameter)
+        {
+            return DeletingCourse is not null;
+        }
+
+        public override async Task ExecuteAsync(object? parameter)
+        {
+            // ToDo: Add store in database
+            if (DeletingCourse is null)
+            {
+                return;
+            }
+
+            Guid courseId = DeletingCourse.CourseId;
+            
+            try
+            {
+                await _courseStore.Delete(courseId);
+            }
+            catch (Exception) { /*ToDo: Write validation message*/ }
+        }
+    }
+}
