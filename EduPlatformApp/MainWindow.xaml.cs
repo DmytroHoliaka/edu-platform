ca99d9ae7ee71c64da3ad1482c8e32c3a82b06c4
HoliakaDmytro <holidim39@gmail.com>
2024-04-14 14:21:06 +0300

    General: Add various width for different modals

--- EduPlatformApp/MainWindow.xaml.cs
+++ EduPlatformApp/MainWindow.xaml.cs
@@ -1,5 +1,10 @@
 ﻿using EduPlatform.WPF.ViewModels;
+using EduPlatform.WPF.ViewModels.CoursesViewModels;
+using EduPlatform.WPF.ViewModels.GroupsViewModels;
+using EduPlatform.WPF.ViewModels.StudentsViewModels;
+using EduPlatform.WPF.ViewModels.TeachersViewModels;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace EduPlatform.WPF
 {
@@ -11,6 +16,29 @@ namespace EduPlatform.WPF
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ModalControl_ContentChanged(object sender, RoutedEventArgs e)
+        {
+            if (sender is ContentControl contentControl)
+            {
+                switch (contentControl.Content)
+                {
+                    case CreateCourseViewModel _:
+                    case UpdateCourseViewModel _:
+                    case CreateStudentViewModel _:
+                    case UpdateStudentViewModel _:
+                    case CreateTeacherViewModel _:
+                    case UpdateTeacherViewModel _:
+                        contentControl.Width = 490;
+                        break;
+
+                    case CreateGroupViewModel _:
+                    case UpdateGroupViewModel _:
+                        contentControl.Width = 940;
+                        break;
+                }
+            }
         }
     }
 }
