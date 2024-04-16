e46eb3db82d8e5bdde1f93af0085ded31c694511
HoliakaDmytro <holidim39@gmail.com>
2024-04-16 09:41:50 +0300

    EntityFramework: Add teacher dto

---
+++ EduPlatform.EntityFramework/DTOs/TeacherDto.cs
@@ -0,0 +1,11 @@
+﻿namespace EduPlatform.EntityFramework.DTOs
+{
+    public class TeacherDto
+    {
+        public Guid TeacherId { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+
+        public ICollection<GroupDto> Groups { get; set; } = [];
+    }
+}
