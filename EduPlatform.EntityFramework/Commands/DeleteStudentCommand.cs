ab1daeb3387014774d1a4ec1293b9078c594ae9b
HoliakaDmytro <holidim39@gmail.com>
2024-04-16 09:45:15 +0300

    EntityFramework: Add implementation of IDeleteStudentCommand

---
+++ EduPlatform.EntityFramework/Commands/DeleteStudentCommand.cs
@@ -0,0 +1,26 @@
+﻿using EduPlatform.Domain.Commands;
+using EduPlatform.EntityFramework.DTOs;
+
+namespace EduPlatform.EntityFramework.Commands
+{
+    public class DeleteStudentCommand : DataOperationBase, IDeleteStudentCommand
+    {
+        public DeleteStudentCommand(EduPlatformDbContextFactory contextFactory)
+            : base(contextFactory)
+        { }
+
+        public async Task ExecuteAsync(Guid studentId)
+        {
+            using (EduPlatformDbContext context = _contextFactory.Create())
+            {
+                StudentDto studentDto = new()
+                {
+                    StudentId = studentId
+                };
+
+                context.Students.Remove(studentDto);
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
