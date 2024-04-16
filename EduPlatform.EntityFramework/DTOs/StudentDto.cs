d3a02c4bf1a657bfe1315437b0e2605ffff8a381
HoliakaDmytro <holidim39@gmail.com>
2024-04-16 09:41:44 +0300

    EntityFramework: Add student dto

---
+++ EduPlatform.EntityFramework/DTOs/StudentDto.cs
@@ -0,0 +1,14 @@
+﻿using EduPlatform.Domain.Models;
+
+namespace EduPlatform.EntityFramework.DTOs
+{
+    public class StudentDto
+    {
+        public Guid StudentId { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+
+        public Guid? GroupId { get; set; }
+        public Group? Group { get; set; }
+    }
+}
