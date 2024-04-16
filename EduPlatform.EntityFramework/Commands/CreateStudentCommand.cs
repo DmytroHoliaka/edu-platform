4560ee62c86585ee4fe11b42c60b23b88173fbbc
HoliakaDmytro <holidim39@gmail.com>
2024-04-16 09:44:56 +0300

    EntityFramework: Add implementation of ICreateStudentCommand

---
+++ EduPlatform.EntityFramework/Commands/CreateStudentCommand.cs
@@ -0,0 +1,32 @@
+﻿using EduPlatform.Domain.Commands;
+using EduPlatform.Domain.Models;
+using EduPlatform.EntityFramework.DTOs;
+
+namespace EduPlatform.EntityFramework.Commands
+{
+    public class CreateStudentCommand : DataOperationBase, ICreateStudentCommand
+    {
+        public CreateStudentCommand(EduPlatformDbContextFactory contextFactory) 
+            : base(contextFactory)
+        { }
+
+        public async Task ExecuteAsync(Student newStudent)
+        {
+            using (EduPlatformDbContext context = _contextFactory.Create())
+            {
+                StudentDto studentDto = new()
+                {
+                    StudentId = newStudent.StudentId,
+                    FirstName = newStudent.FirstName,
+                    LastName = newStudent.LastName,
+                    GroupId = newStudent.GroupId,
+                    Group = newStudent.Group,
+                };
+
+                context.Students.Add(studentDto);
+
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
