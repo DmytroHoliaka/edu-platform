35e55ec22fd5f283716681d10e66dd135340f9cb
HoliakaDmytro <holidim39@gmail.com>
2024-04-16 08:42:29 +0300

    Commands: Add update student command interface

---
+++ EduPlatform.Domain/Commands/IUpdateStudentCommand.cs.cs
@@ -0,0 +1,9 @@
+﻿using EduPlatform.Domain.Models;
+
+namespace EduPlatform.Domain.Commands
+{
+    internal interface IUpdateStudentCommand
+    {
+        Task Execute(Student targetStudent);
+    }
+}
