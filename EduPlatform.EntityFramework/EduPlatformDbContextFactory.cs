b3c29f7373a4f1c34646c1bccbe4928cb0e118e8
HoliakaDmytro <holidim39@gmail.com>
2024-04-16 09:40:45 +0300

    EntityFramework: Add database context factory class

---
+++ EduPlatform.EntityFramework/EduPlatformDbContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EduPlatform.EntityFramework
+{
+    public class EduPlatformDbContextFactory
+    {
+        private readonly DbContextOptions _options;
+
+        public EduPlatformDbContextFactory(DbContextOptions options)
+        {
+            _options = options;
+        }
+
+        public EduPlatformDbContext Create()
+        {
+            return new EduPlatformDbContext(_options);
+        }
+    }
+}
