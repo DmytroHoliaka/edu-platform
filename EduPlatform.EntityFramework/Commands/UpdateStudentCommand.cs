514783460c65bc5490ffd885b01e735c89f1cb01
HoliakaDmytro <holidim39@gmail.com>
2024-04-16 09:45:21 +0300

    EntityFramework: Add implementation of IUpdateStudentCommand

---
+++ EduPlatform.EntityFramework/Commands/UpdateStudentCommand.cs
@@ -0,0 +1,33 @@
+﻿using EduPlatform.Domain.Commands;
+using EduPlatform.Domain.Models;
+using EduPlatform.EntityFramework.DTOs;
+using Microsoft.EntityFrameworkCore.Storage.Json;
+
+namespace EduPlatform.EntityFramework.Commands
+{
+    public class UpdateStudentCommand : DataOperationBase, IUpdateStudentCommand
+    {
+        public UpdateStudentCommand(EduPlatformDbContextFactory contextFactory) 
+            : base(contextFactory)
+        { }
+
+        public async Task ExecuteAsync(Student targetStudent)
+        {
+            using (EduPlatformDbContext context = _contextFactory.Create())
+            {
+                StudentDto targetStudentDto = new()
+                {
+                    StudentId = targetStudent.StudentId,
+                    FirstName = targetStudent.FirstName,
+                    LastName = targetStudent.LastName,
+                    GroupId = targetStudent.GroupId,
+                    Group = targetStudent.Group,
+                };
+
+                context.Students.Update(targetStudentDto);
+
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
