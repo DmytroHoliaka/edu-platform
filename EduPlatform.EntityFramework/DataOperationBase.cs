c0cefb6594dbae0e42376c782a04fe01b8440f76
HoliakaDmytro <holidim39@gmail.com>
2024-04-16 09:44:17 +0300

    EntityFramework: Add base class for all database operations

---
+++ EduPlatform.EntityFramework/DataOperationBase.cs
@@ -0,0 +1,12 @@
+﻿namespace EduPlatform.EntityFramework
+{
+    public class DataOperationBase
+    {
+        protected readonly EduPlatformDbContextFactory _contextFactory;
+
+        public DataOperationBase(EduPlatformDbContextFactory contextFactory)
+        {
+            _contextFactory = contextFactory;
+        }
+    }
+}
