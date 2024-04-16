7229aa249d25b02f21ae005a799ea1eccc12f314
HoliakaDmytro <holidim39@gmail.com>
2024-04-16 09:41:39 +0300

    EntityFramework: Add group dto

---
+++ EduPlatform.EntityFramework/DTOs/GroupDto.cs
@@ -0,0 +1,14 @@
+﻿namespace EduPlatform.EntityFramework.DTOs
+{
+    public class GroupDto
+    {
+        public Guid GroupId { get; set; }
+        public string? Name { get; set; }
+
+        public Guid? CourseId { get; set; }
+        public CourseDto? Course { get; set; }
+
+        public ICollection<TeacherDto> Teachers { get; set; } = [];
+        public ICollection<StudentDto> Students { get; set; } = [];
+    }
+}
