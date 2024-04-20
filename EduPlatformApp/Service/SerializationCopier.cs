c3418edc2fcd62737721f90770f83e9ef5a62380
HoliakaDmytro <holidim39@gmail.com>
2024-04-20 12:24:19 +0300

    Service: Add class for getting deep copy of any objects

---
+++ EduPlatformApp/Service/SerializationCopier.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+
+namespace EduPlatform.WPF.Service
+{
+    public class SerializationCopier
+    {
+        public static T? DeepCopy<T>(T obj)
+        {
+            JsonSerializerSettings settings = new() 
+            { 
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore 
+            };
+
+            string json = JsonConvert.SerializeObject(obj, settings);
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+    }
+}
