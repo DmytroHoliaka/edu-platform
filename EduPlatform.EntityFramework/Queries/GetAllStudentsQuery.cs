2fab99979a4b2067ea1fbd0be38d6c1f95f57cfa
HoliakaDmytro <holidim39@gmail.com>
2024-04-20 12:19:18 +0300

    EntityFramework.Queries: Add query to get all students from database

--- EduPlatform.EntityFramework/Queries/GetAllStudentsQuery.cs
+++ EduPlatform.EntityFramework/Queries/GetAllStudentsQuery.cs
@@ -1,30 +1,18 @@
 ﻿using EduPlatform.Domain.Models;
 using EduPlatform.Domain.Queries;
-using EduPlatform.EntityFramework.DTOs;
+using EduPlatform.EntityFramework.DbContextConfigurations;
 using Microsoft.EntityFrameworkCore;
 
 namespace EduPlatform.EntityFramework.Queries
 {
-    public class GetAllStudentsQuery : DataOperationBase, IGetAllStudentsQuery
+    public class GetAllStudentsQuery(EduPlatformDbContextFactory contextFactory) : IGetAllStudentsQuery
     {
-        public GetAllStudentsQuery(EduPlatformDbContextFactory contextFactory)
-            : base(contextFactory)
-        { }
-
         public async Task<IEnumerable<Student>> ExecuteAsync()
         {
-            using (EduPlatformDbContext context = _contextFactory.Create())
+            using (EduPlatformDbContext context = contextFactory.Create())
             {
-                IEnumerable<StudentDto> studentDtos = await context.Students.ToListAsync();
-
-                return studentDtos.Select(dto => new Student()
-                {
-                    StudentId = dto.StudentId,
-                    FirstName = dto.FirstName,
-                    LastName = dto.LastName,
-                    GroupId = dto.GroupId,
-                    Group = dto.Group,
-                });
+                IEnumerable<Student> students = await context.Students.ToListAsync();
+                return students;
             }
         }
     }
