c3267819e31e33edc7b8e169abdcef9543027b6f
HoliakaDmytro <holidim39@gmail.com>
2024-04-20 12:07:50 +0300

    Domain.Commands: Add interface to create course

---
+++ EduPlatform.Domain/Commands/ICreateCourseCommand.cs
@@ -0,0 +1,9 @@
+﻿using EduPlatform.Domain.Models;
+
+namespace EduPlatform.Domain.Commands
+{
+    public interface ICreateCourseCommand
+    {
+        Task ExecuteAsync(Course newCourse);
+    }
+}
