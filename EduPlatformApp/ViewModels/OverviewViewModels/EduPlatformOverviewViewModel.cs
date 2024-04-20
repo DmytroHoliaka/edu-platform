39524666ab5f90b201a43f72f2d163225e535f0f
HoliakaDmytro <holidim39@gmail.com>
2024-04-20 12:26:15 +0300

    OverviewViewModels: Remove unnecessary usings

--- EduPlatformApp/ViewModels/OverviewViewModels/EduPlatformOverviewViewModel.cs
+++ EduPlatformApp/ViewModels/OverviewViewModels/EduPlatformOverviewViewModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace EduPlatform.WPF.ViewModels.GeneralViewModels.OverviewViewModel
+﻿namespace EduPlatform.WPF.ViewModels.GeneralViewModels.OverviewViewModel
 {
     public class EduPlatformOverviewViewModel : ViewModelBase
     {
