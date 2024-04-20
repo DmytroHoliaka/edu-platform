215f284e0ea9d30fbc02147545953318acfe6b79
HoliakaDmytro <holidim39@gmail.com>
2024-04-20 12:14:07 +0300

    EntityFramework.Commands: Add command to create course in database

---
+++ EduPlatform.EntityFramework/Commands/CreateCourseCommand.cs
@@ -0,0 +1,21 @@
+﻿using EduPlatform.Domain.Commands;
+using EduPlatform.Domain.Models;
+using EduPlatform.EntityFramework.DbContextConfigurations;
+using EduPlatform.EntityFramework.Service;
+
+namespace EduPlatform.EntityFramework.Commands
+{
+    public class CreateCourseCommand(EduPlatformDbContextFactory contextFactory) : ICreateCourseCommand
+    {
+        public async Task ExecuteAsync(Course newCourse)
+        {
+            using (EduPlatformDbContext context = contextFactory.Create())
+            {
+                EntityMapper.SetCourseDbRelationships(context, newCourse);
+
+                await context.Courses.AddAsync(newCourse);
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
