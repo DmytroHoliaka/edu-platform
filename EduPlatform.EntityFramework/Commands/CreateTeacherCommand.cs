47c184c87d9d1f0145f9083b7f72643d58635f7c
HoliakaDmytro <holidim39@gmail.com>
2024-04-20 12:16:10 +0300

    EntityFramework.Commands: Add command to create teacher in database

---
+++ EduPlatform.EntityFramework/Commands/CreateTeacherCommand.cs
@@ -0,0 +1,21 @@
+﻿using EduPlatform.Domain.Commands;
+using EduPlatform.Domain.Models;
+using EduPlatform.EntityFramework.DbContextConfigurations;
+using EduPlatform.EntityFramework.Service;
+
+namespace EduPlatform.EntityFramework.Commands
+{
+    public class CreateTeacherCommand(EduPlatformDbContextFactory contextFactory) : ICreateTeacherCommand
+    {
+        public async Task ExecuteAsync(Teacher newTeacher)
+        {
+            using (EduPlatformDbContext context = contextFactory.Create())
+            {
+                EntityMapper.SetTeacherDbRelationships(context, newTeacher);
+                
+                await context.Teachers.AddAsync(newTeacher);
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
