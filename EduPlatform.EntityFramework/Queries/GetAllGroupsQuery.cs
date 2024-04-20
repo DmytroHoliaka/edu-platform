32af5fa675e3486586a15abdeef58f85c3a9e1a6
HoliakaDmytro <holidim39@gmail.com>
2024-04-20 12:19:12 +0300

    EntityFramework.Queries: Add query to get all groups from database

---
+++ EduPlatform.EntityFramework/Queries/GetAllGroupsQuery.cs
@@ -0,0 +1,19 @@
+﻿using EduPlatform.Domain.Models;
+using EduPlatform.Domain.Queries;
+using EduPlatform.EntityFramework.DbContextConfigurations;
+using Microsoft.EntityFrameworkCore;
+
+namespace EduPlatform.EntityFramework.Queries
+{
+    public class GetAllGroupsQuery(EduPlatformDbContextFactory contextFactory) : IGetAllGroupsQuery
+    {
+        public async Task<IEnumerable<Group>> ExecuteAsync()
+        {
+            using (EduPlatformDbContext context = contextFactory.Create())
+            {
+                IEnumerable<Group> groups = await context.Groups.ToListAsync();
+                return groups;
+            }
+        }
+    }
+}
