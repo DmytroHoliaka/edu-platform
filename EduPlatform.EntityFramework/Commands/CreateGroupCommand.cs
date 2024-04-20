cc1162138f2e4b74f506a685fd35b1acccecb0eb
HoliakaDmytro <holidim39@gmail.com>
2024-04-20 12:14:18 +0300

    EntityFramework.Commands: Add command to create group in database

---
+++ EduPlatform.EntityFramework/Commands/CreateGroupCommand.cs
@@ -0,0 +1,21 @@
+﻿using EduPlatform.Domain.Commands;
+using EduPlatform.Domain.Models;
+using EduPlatform.EntityFramework.DbContextConfigurations;
+using EduPlatform.EntityFramework.Service;
+
+namespace EduPlatform.EntityFramework.Commands
+{
+    public class CreateGroupCommand(EduPlatformDbContextFactory contextFactory) : ICreateGroupCommand
+    {
+        public async Task ExecuteAsync(Group newGroup)
+        {
+            using (EduPlatformDbContext context = contextFactory.Create())
+            {
+                EntityMapper.SetGroupDbRelationships(context, newGroup);
+
+                await context.Groups.AddAsync(newGroup);
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
