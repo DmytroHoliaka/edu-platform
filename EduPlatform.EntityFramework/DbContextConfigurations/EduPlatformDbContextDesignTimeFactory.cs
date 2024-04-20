1c539028b17e07da1bae4e342d03606e1e1ab9b7
HoliakaDmytro <holidim39@gmail.com>
2024-04-20 12:13:04 +0300

    EntityFramework: DbContextConfigurations: Add factory class for migration purpose

---
+++ EduPlatform.EntityFramework/DbContextConfigurations/EduPlatformDbContextDesignTimeFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace EduPlatform.EntityFramework.DbContextConfigurations
+{
+    public class EduPlatformDbContextDesignTimeFactory : IDesignTimeDbContextFactory<EduPlatformDbContext>
+    {
+        public EduPlatformDbContext CreateDbContext(string[] args)
+        {
+            // ToDo: Use JSON configuration file #2
+            const string connectionString = @"Server=(localdb)\mssqllocaldb;Database=EduPlatform;Trusted_Connection=True;";
+            DbContextOptions options = new DbContextOptionsBuilder()
+                .UseSqlServer(connectionString).Options;
+            return new EduPlatformDbContext(options);
+        }
+    }
+}
