e96a8a8b1138ffa990b9ad3682abcf349751eed9
HoliakaDmytro <holidim39@gmail.com>
2024-04-20 12:13:15 +0300

    EntityFramework: DbContextConfigurations: Add main data context

---
+++ EduPlatform.EntityFramework/DbContextConfigurations/EduPlatformDbContext.cs
@@ -0,0 +1,31 @@
+﻿using EduPlatform.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EduPlatform.EntityFramework.DbContextConfigurations
+{
+    public class EduPlatformDbContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<Course> Courses => Set<Course>();
+        public DbSet<Group> Groups => Set<Group>();
+        public DbSet<Student> Students => Set<Student>();
+        public DbSet<Teacher> Teachers => Set<Teacher>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Group>()
+                .HasMany(g => g.Teachers)
+                .WithMany(t => t.Groups)
+                .UsingEntity<Dictionary<string, object>>(
+                    j => j.HasOne<Teacher>()
+                          .WithMany()
+                          .HasForeignKey("TeacherId"),
+
+                    j => j.HasOne<Group>()
+                          .WithMany()
+                          .HasForeignKey("GroupId"),
+
+                    j => j.ToTable("GroupTeacherMap")
+                );
+        }
+    }
+}
