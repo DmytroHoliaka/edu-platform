07bb7b3f64b5c8d9d0c3a92a124e91c1e569c7f0
HoliakaDmytro <holidim39@gmail.com>
2024-04-20 12:16:59 +0300

    EntityFramework.Commands: Add command to update group in database

---
+++ EduPlatform.EntityFramework/Commands/UpdateGroupCommand.cs
@@ -0,0 +1,40 @@
+﻿using EduPlatform.Domain.Commands;
+using EduPlatform.Domain.Models;
+using EduPlatform.EntityFramework.DbContextConfigurations;
+using EduPlatform.EntityFramework.Service;
+using Microsoft.EntityFrameworkCore;
+
+namespace EduPlatform.EntityFramework.Commands
+{
+    public class UpdateGroupCommand(EduPlatformDbContextFactory contextFactory) : IUpdateGroupCommand
+    {
+        public async Task ExecuteAsync(Group targetGroup)
+        {
+            using (EduPlatformDbContext context = contextFactory.Create())
+            {
+                EntityMapper.SetGroupDbRelationships(context, targetGroup);
+
+                Group? sourceGroupFromDb = context.Groups
+                    .Include(g => g.Course)
+                    .Include(g => g.Teachers)
+                    .Include(g => g.Students)
+                    .FirstOrDefault(g => g.GroupId == targetGroup.GroupId);
+
+                if (sourceGroupFromDb is null)
+                {
+                    CreateGroupCommand createGroupCommand = new(contextFactory);
+                    await createGroupCommand.ExecuteAsync(targetGroup);
+                }
+                else
+                {
+                    sourceGroupFromDb.Name = targetGroup.Name;
+                    sourceGroupFromDb.Course = targetGroup.Course;
+                    sourceGroupFromDb.Teachers = targetGroup.Teachers;
+                    sourceGroupFromDb.Students = targetGroup.Students;
+                }
+
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
