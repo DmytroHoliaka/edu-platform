73f8004abb5cec2c88cb33c569d2bdda8a2b05cd
HoliakaDmytro <holidim39@gmail.com>
2024-04-20 12:16:38 +0300

    EntityFramework.Commands: Add command to delete teacher in database

---
+++ EduPlatform.EntityFramework/Commands/DeleteTeacherCommand.cs
@@ -0,0 +1,23 @@
+﻿using EduPlatform.Domain.Commands;
+using EduPlatform.Domain.Models;
+using EduPlatform.EntityFramework.DbContextConfigurations;
+
+namespace EduPlatform.EntityFramework.Commands
+{
+    public class DeleteTeacherCommand(EduPlatformDbContextFactory contextFactory) : IDeleteTeacherCommand
+    {
+        public async Task ExecuteAsync(Guid teacherId)
+        {
+            using (EduPlatformDbContext context = contextFactory.Create())
+            {
+                Teacher teacher = new()
+                {
+                    TeacherId= teacherId
+                };
+
+                context.Teachers.Remove(teacher);
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
