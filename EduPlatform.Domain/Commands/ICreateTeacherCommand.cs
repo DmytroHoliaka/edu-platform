c3b3dcb0909a7886278e5727ba34e64a5e2dd2db
HoliakaDmytro <holidim39@gmail.com>
2024-04-20 12:08:11 +0300

    Domain.Commands: Add interface to create teacher

---
+++ EduPlatform.Domain/Commands/ICreateTeacherCommand.cs
@@ -0,0 +1,9 @@
+﻿using EduPlatform.Domain.Models;
+
+namespace EduPlatform.Domain.Commands
+{
+    public interface ICreateTeacherCommand
+    {
+        Task ExecuteAsync(Teacher newTeacher);
+    }
+}
