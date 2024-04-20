6c9c27d55aa995fee0556c3fdf883ed6c035fabe
HoliakaDmytro <holidim39@gmail.com>
2024-04-20 12:11:16 +0300

    Domain.Queries: Add interface to get all groups

---
+++ EduPlatform.Domain/Queries/IGetAllGroupsQuery.cs
@@ -0,0 +1,9 @@
+﻿using EduPlatform.Domain.Models;
+
+namespace EduPlatform.Domain.Queries
+{
+    public interface IGetAllGroupsQuery
+    {
+        Task<IEnumerable<Group>> ExecuteAsync();
+    }
+}
