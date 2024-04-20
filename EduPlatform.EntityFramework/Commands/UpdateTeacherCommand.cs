58a9458ed6c75bb14df12217176014ba8897202f
HoliakaDmytro <holidim39@gmail.com>
2024-04-20 12:17:12 +0300

    EntityFramework.Commands: Add command to update teacher in database

---
+++ EduPlatform.EntityFramework/Commands/UpdateTeacherCommand.cs
@@ -0,0 +1,37 @@
+﻿using EduPlatform.Domain.Commands;
+using EduPlatform.Domain.Models;
+using EduPlatform.EntityFramework.DbContextConfigurations;
+using EduPlatform.EntityFramework.Service;
+using Microsoft.EntityFrameworkCore;
+
+namespace EduPlatform.EntityFramework.Commands
+{
+    public class UpdateTeacherCommand(EduPlatformDbContextFactory contextFactory) : IUpdateTeacherCommand
+    {
+        public async Task ExecuteAsync(Teacher targetTeacher)
+        {
+            using (EduPlatformDbContext context = contextFactory.Create())
+            {
+                EntityMapper.SetTeacherDbRelationships(context, targetTeacher);
+                
+                Teacher? sourceTeacher = context.Teachers
+                    .Include(t => t.Groups)
+                    .FirstOrDefault(t => t.TeacherId == targetTeacher.TeacherId);
+
+                if (sourceTeacher is null)
+                {
+                    CreateTeacherCommand createTeacherCommand = new(contextFactory);
+                    await createTeacherCommand.ExecuteAsync(targetTeacher);
+                }
+                else
+                {
+                    sourceTeacher.FirstName = targetTeacher.FirstName;
+                    sourceTeacher.LastName = targetTeacher.LastName;
+                    sourceTeacher.Groups = targetTeacher.Groups;
+                }
+
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
