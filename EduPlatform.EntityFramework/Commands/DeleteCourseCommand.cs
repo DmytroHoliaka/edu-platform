3f40329a616af33d8ba6ffd8cdc823ab863490f7
HoliakaDmytro <holidim39@gmail.com>
2024-04-20 12:16:19 +0300

    EntityFramework.Commands: Add command to delete course in database

---
+++ EduPlatform.EntityFramework/Commands/DeleteCourseCommand.cs
@@ -0,0 +1,23 @@
+﻿using EduPlatform.Domain.Commands;
+using EduPlatform.Domain.Models;
+using EduPlatform.EntityFramework.DbContextConfigurations;
+
+namespace EduPlatform.EntityFramework.Commands
+{
+    public class DeleteCourseCommand(EduPlatformDbContextFactory contextFactory) : IDeleteCourseCommand
+    {
+        public async Task ExecuteAsync(Guid courseId)
+        {
+            using (EduPlatformDbContext context = contextFactory.Create())
+            {
+                Course course = new()
+                {
+                    CourseId = courseId
+                };
+
+                context.Courses.Remove(course);
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
