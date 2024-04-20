e872e7b40b8b86a9bda4af710f3ab6bd79e0df87
HoliakaDmytro <holidim39@gmail.com>
2024-04-20 12:19:05 +0300

    EntityFramework.Queries: Add query to get all courses from database

---
+++ EduPlatform.EntityFramework/Queries/GetAllCoursesQuery.cs
@@ -0,0 +1,19 @@
+﻿using EduPlatform.Domain.Models;
+using EduPlatform.Domain.Queries;
+using EduPlatform.EntityFramework.DbContextConfigurations;
+using Microsoft.EntityFrameworkCore;
+
+namespace EduPlatform.EntityFramework.Queries
+{
+    public class GetAllCoursesQuery(EduPlatformDbContextFactory contextFactory) : IGetAllCoursesQuery
+    {
+        public async Task<IEnumerable<Course>> ExecuteAsync()
+        {
+            using (EduPlatformDbContext context = contextFactory.Create())
+            {
+                IEnumerable<Course> courses = await context.Courses.ToListAsync();
+                return courses;
+            }
+        }
+    }
+}
