18d8ea1eb5755cf94a826782550ea3ca86730ed8
HoliakaDmytro <holidim39@gmail.com>
2024-04-20 12:16:25 +0300

    EntityFramework.Commands: Add command to delete group in database

---
+++ EduPlatform.EntityFramework/Commands/DeleteGroupCommand.cs
@@ -0,0 +1,23 @@
+﻿using EduPlatform.Domain.Commands;
+using EduPlatform.Domain.Models;
+using EduPlatform.EntityFramework.DbContextConfigurations;
+
+namespace EduPlatform.EntityFramework.Commands
+{
+    public class DeleteGroupCommand(EduPlatformDbContextFactory contextFactory) : IDeleteGroupCommand
+    {
+        public async Task ExecuteAsync(Guid groupId)
+        {
+            using (EduPlatformDbContext context = contextFactory.Create())
+            {
+                Group group = new()
+                {
+                    GroupId = groupId
+                };
+
+                context.Groups.Remove(group);
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
