a0cf80710ccb9dca64f617d420ab668edf2a90d1
HoliakaDmytro <holidim39@gmail.com>
2024-04-21 09:24:02 +0300

    CourseCommands: Add command to load courses

---
+++ EduPlatformApp/Commands/CourseCommands/LoadCoursesCommand.cs
@@ -0,0 +1,21 @@
+﻿using EduPlatform.WPF.Commands.BaseCommands;
+using EduPlatform.WPF.Stores;
+
+namespace EduPlatform.WPF.Commands.CourseCommands
+{
+    public class LoadCoursesCommand(CourseStore courseStore) : AsyncCommandBase
+    {
+        public override async Task ExecuteAsync(object? parameter)
+        {
+            try
+            {
+                await courseStore.Load();
+            }
+            catch (Exception)
+            {
+                // ToDo: Write correct exception handling
+                throw;
+            }
+        }
+    }
+}
