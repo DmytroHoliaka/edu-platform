19de77896eca3640cfab48807937c3de138f0692
HoliakaDmytro <holidim39@gmail.com>
2024-04-21 10:09:43 +0300

    Stores: Add event TeacherLoaded and sequence Teacher in TeacherStore

--- EduPlatformApp/Stores/TeacherStore.cs
+++ EduPlatformApp/Stores/TeacherStore.cs
@@ -1,6 +1,7 @@
 ﻿using EduPlatform.Domain.Commands;
 using EduPlatform.Domain.Models;
 using EduPlatform.Domain.Queries;
+Stores: Add event GroupsLoaded and sequence Groups in GroupStoreusing EduPlatform.EntityFramework.Queries;
 using EduPlatform.WPF.Service;
 
 namespace EduPlatform.WPF.Stores
@@ -11,27 +12,57 @@ namespace EduPlatform.WPF.Stores
         IUpdateTeacherCommand updateTeacherCommand,
         IDeleteTeacherCommand deleteTeacherCommand)
     {
+        public event Action? TeachersLoaded;
         public event Action<Teacher>? TeacherAdded;
         public event Action<Teacher>? TeacherUpdated;
         public event Action<Guid>? TeacherDeleted;
 
-        private readonly IGetAllTeachersQuery _getAllTeachersQuery = getAllTeachersQuery;
+        public IEnumerable<Teacher> Teachers => _teachers;
+        private readonly List<Teacher> _teachers = [];
+
+        public async Task Load()
+        {
+            IEnumerable<Teacher> teachersFromDb = await getAllTeachersQuery.ExecuteAsync();
+
+            _teachers.Clear();
+            _teachers.AddRange(teachersFromDb);
+
+            TeachersLoaded?.Invoke();
+        }
 
         public async Task Add(Teacher newTeacher)
         {
             await createTeacherCommand.ExecuteAsync(SerializationCopier.DeepCopy(newTeacher)!);
+
+            _teachers.Add(newTeacher);
+
             TeacherAdded?.Invoke(newTeacher);
         }
 
         public async Task Update(Teacher targetTeacher)
         {
             await updateTeacherCommand.ExecuteAsync(SerializationCopier.DeepCopy(targetTeacher)!);
+
+            int index = _teachers.FindIndex(t => t.TeacherId == targetTeacher.TeacherId);
+
+            if (index == -1)
+            {
+                _teachers.Add(targetTeacher);
+            }
+            else
+            {
+                _teachers[index] = targetTeacher;
+            }
+
             TeacherUpdated?.Invoke(targetTeacher);
         }
 
         public async Task Delete(Guid teacherId)
         {
             await deleteTeacherCommand.ExecuteAsync(teacherId);
+
+            _teachers.RemoveAll(t => t.TeacherId == teacherId);
+
             TeacherDeleted?.Invoke(teacherId);
         }
     }
