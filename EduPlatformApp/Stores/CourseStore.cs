afec3ef03d3d91016cfc94e927b67616fbac02a7
HoliakaDmytro <holidim39@gmail.com>
2024-04-21 09:24:30 +0300

    Stores: Add event CoursesLoaded and Courses sequence in CourseStore

--- EduPlatformApp/Stores/CourseStore.cs
+++ EduPlatformApp/Stores/CourseStore.cs
@@ -1,6 +1,7 @@
 ﻿using EduPlatform.Domain.Commands;
 using EduPlatform.Domain.Models;
 using EduPlatform.Domain.Queries;
+using EduPlatform.EntityFramework.Queries;
 using EduPlatform.WPF.Service;
 
 namespace EduPlatform.WPF.Stores
@@ -11,27 +12,57 @@ namespace EduPlatform.WPF.Stores
         IUpdateCourseCommand updateCourseCommand,
         IDeleteCourseCommand deleteCourseCommand)
     {
+        public event Action? CoursesLoaded;
         public event Action<Course>? CourseAdded;
         public event Action<Course>? CourseUpdated;
         public event Action<Guid>? CourseDeleted;
 
-        private readonly IGetAllCoursesQuery _getAllCoursesQuery = getAllCoursesQuery;
+        public IEnumerable<Course> Courses => _courses;
+        private readonly List<Course> _courses = [];
+
+        public async Task Load()
+        {
+            IEnumerable<Course> coursesFromDb = await getAllCoursesQuery.ExecuteAsync();
+
+            _courses.Clear();
+            _courses.AddRange(coursesFromDb);
+
+            CoursesLoaded?.Invoke();
+        }
 
         public async Task Add(Course newCourse)
         {
             await createCourseCommand.ExecuteAsync(SerializationCopier.DeepCopy(newCourse)!);
+
+            _courses.Add(newCourse);
+
             CourseAdded?.Invoke(newCourse);
         }
 
         public async Task Update(Course targetCourse)
         {
             await updateCourseCommand.ExecuteAsync(SerializationCopier.DeepCopy(targetCourse)!);
+
+            int index = _courses.FindIndex(c => c.CourseId == targetCourse.CourseId);
+
+            if (index == -1)
+            {
+                _courses.Add(targetCourse);
+            }
+            else
+            {
+                _courses[index] = targetCourse;
+            }
+
             CourseUpdated?.Invoke(targetCourse);
         }
 
         public async Task Delete(Guid courseId)
         {
             await deleteCourseCommand.ExecuteAsync(courseId);
+
+            _courses.RemoveAll(c => c.CourseId == courseId);
+
             CourseDeleted?.Invoke(courseId);
         }
     }
