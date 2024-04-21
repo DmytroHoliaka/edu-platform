a61009df7d969afc5e84000147e429007f5543a3
HoliakaDmytro <holidim39@gmail.com>
2024-04-21 10:09:27 +0300

    Stores: Add event GroupsLoaded and sequence Groups in GroupStore

--- EduPlatformApp/Stores/GroupStore.cs
+++ EduPlatformApp/Stores/GroupStore.cs
@@ -11,28 +11,57 @@ namespace EduPlatform.WPF.Stores
         IUpdateGroupCommand updateGroupCommand,
         IDeleteGroupCommand deleteGroupCommand)
     {
+        public event Action? GroupsLoaded;
         public event Action<Group>? GroupAdded;
         public event Action<Group>? GroupUpdated;
         public event Action<Guid>? GroupDeleted;
 
-        private readonly IGetAllGroupsQuery _getAllGroupsQuery = getAllGroupsQuery;
+        public IEnumerable<Group> Groups => _groups;
+        private readonly List<Group> _groups = [];
 
+        public async Task Load()
+        {
+            IEnumerable<Group> groupsFromDb = await getAllGroupsQuery.ExecuteAsync();
+
+            _groups.Clear();
+            _groups.AddRange(groupsFromDb);
+
+            GroupsLoaded?.Invoke();
+        }
 
         public async Task Add(Group newGroup)
         {
             await createGroupCommand.ExecuteAsync(SerializationCopier.DeepCopy(newGroup)!);
+
+            _groups.Add(newGroup);
+
             GroupAdded?.Invoke(newGroup);
         }
 
         public async Task Update(Group targetGroup)
         {
             await updateGroupCommand.ExecuteAsync(SerializationCopier.DeepCopy(targetGroup)!);
+
+            int index = _groups.FindIndex(g => g.GroupId == targetGroup.GroupId);
+
+            if (index == -1)
+            {
+                _groups.Add(targetGroup);
+            }
+            else
+            {
+                _groups[index] = targetGroup;
+            }
+
             GroupUpdated?.Invoke(targetGroup);
         }
 
         public async Task Delete(Guid groupId)
         {
             await deleteGroupCommand.ExecuteAsync(groupId);
+
+            _groups.RemoveAll(g => g.GroupId == groupId);
+
             GroupDeleted?.Invoke(groupId);
         }
     }
