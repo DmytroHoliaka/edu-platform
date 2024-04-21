7f26d076895d3753f19beafad06a0ea81fb1dbc1
HoliakaDmytro <holidim39@gmail.com>
2024-04-21 10:08:57 +0300

    TeacherCommands: Add command to load teachers

---
+++ EduPlatformApp/Commands/TeacherCommands/LoadTeachersCommand.cs
@@ -0,0 +1,21 @@
+﻿using EduPlatform.WPF.Commands.BaseCommands;
+using EduPlatform.WPF.Stores;
+
+namespace EduPlatform.WPF.Commands.TeacherCommands
+{
+    public class LoadTeachersCommand(TeacherStore teacherStore) : AsyncCommandBase
+    {
+        public override async Task ExecuteAsync(object? parameter)
+        {
+            try
+            {
+                await teacherStore.Load();
+            }
+            catch (Exception)
+            {
+                // ToDo: Write correct exception handling
+                throw;
+            }
+        }
+    }
+}
