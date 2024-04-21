df55302477a34191462899ccaadaf7b91acf1cb4
HoliakaDmytro <holidim39@gmail.com>
2024-04-21 09:12:33 +0300

    StudentCommands: Add command to load students from database

---
+++ EduPlatformApp/Commands/StudentCommands/LoadStudentsCommand.cs
@@ -0,0 +1,21 @@
+﻿using EduPlatform.WPF.Commands.BaseCommands;
+using EduPlatform.WPF.Stores;
+
+namespace EduPlatform.WPF.Commands.StudentCommands
+{
+    public class LoadStudentsCommand(StudentStore studentStore) : AsyncCommandBase
+    {
+        public override async Task ExecuteAsync(object? parameter)
+        {
+            try
+            {
+                await studentStore.Load();
+            }
+            catch (Exception)
+            {
+                // ToDo: Write correct exception handling
+                throw;
+            }
+        }
+    }
+}
