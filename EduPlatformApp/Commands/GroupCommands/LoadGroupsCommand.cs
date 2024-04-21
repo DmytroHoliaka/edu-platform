d8bbfc17c8faf1d4edb8e8f8bf2798272d2dbc83
HoliakaDmytro <holidim39@gmail.com>
2024-04-21 10:08:46 +0300

    GroupCommands: Add command to load groups

---
+++ EduPlatformApp/Commands/GroupCommands/LoadGroupsCommand.cs
@@ -0,0 +1,21 @@
+﻿using EduPlatform.WPF.Commands.BaseCommands;
+using EduPlatform.WPF.Stores;
+
+namespace EduPlatform.WPF.Commands.GroupCommands
+{
+    public class LoadGroupsCommand(GroupStore groupStore) : AsyncCommandBase
+    {
+        public override async Task ExecuteAsync(object? parameter)
+        {
+            try
+            {
+                await groupStore.Load();
+            }
+            catch (Exception)
+            {
+                // ToDo: Write correct exception handling
+                throw;
+            }
+        }
+    }
+}
