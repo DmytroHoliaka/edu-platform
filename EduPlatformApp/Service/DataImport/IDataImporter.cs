cccc493b632955f9c06786e715bbbc5eccca118a
HoliakaDmytro <holidim39@gmail.com>
2024-05-27 14:02:07 +0300

    DataImport: Add interface to import students into group from different formats

---
+++ EduPlatformApp/Service/DataImport/IDataImporter.cs
@@ -0,0 +1,9 @@
+﻿using EduPlatform.WPF.Stores;
+using EduPlatform.WPF.ViewModels.GroupsViewModels;
+
+namespace EduPlatform.WPF.Service.DataImport;
+
+public interface IDataImporter
+{
+    Task ImportStudents(StudentStore studentStore, GroupViewModel groupVM, string? path);
+}
