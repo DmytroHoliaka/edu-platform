f99623cf80b6f2fc6a26a328c9e48e8a89119fe0
HoliakaDmytro <holidim39@gmail.com>
2024-05-27 13:57:41 +0300

    Service: Add service for additinal methods with collections

---
+++ EduPlatformApp/Service/CollectionService.cs
@@ -0,0 +1,23 @@
+﻿using System.CodeDom.Compiler;
+using EduPlatform.WPF.Service.Validators;
+using System.Text;
+
+namespace EduPlatform.WPF.Service;
+
+public static class CollectionService
+{
+    public static string GetString<T>(List<T>? list)
+    {
+        ArgumentNullException.ThrowIfNull(list, nameof(list));
+
+        StringBuilder complexLine = new();
+
+        foreach (T item in list)
+        {
+            complexLine.Append(item);
+            complexLine.Append(Environment.NewLine);
+        }
+
+        return complexLine.ToString();
+    }
+}
