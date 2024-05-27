5b7fa493cd2eb524b903ad6447223d3725c0a6ee
HoliakaDmytro <holidim39@gmail.com>
2024-05-27 14:03:19 +0300

    GroupCommands: Add command to export students into csv file

---
+++ EduPlatformApp/Commands/GroupCommands/ExportStudentsCommand.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows.Forms;
+using EduPlatform.WPF.Commands.BaseCommands;
+using EduPlatform.WPF.Service.DataExport;
+using EduPlatform.WPF.ViewModels.GroupsViewModels;
+
+namespace EduPlatform.WPF.Commands.GroupCommands;
+
+public class ExportStudentsCommand(GroupViewModel groupVM) : AsyncCommandBase
+{
+    public override async Task ExecuteAsync(object? parameter)
+    {
+        CsvExporter exporter = new();
+
+        try
+        {
+            await exporter.ExportStudent(groupVM);
+            MessageBox.Show("Students successfully exported (folder ExportedData in the root directory)");
+        }
+        catch (Exception)
+        {
+            // ToDo: Create correct error handling
+            throw;
+        }
+    }
+}
