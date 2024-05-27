b51c743709986c483f91d69024b10c0ff469661f
HoliakaDmytro <holidim39@gmail.com>
2024-05-27 14:03:28 +0300

    GroupCommands: Add command to import students from csv file

---
+++ EduPlatformApp/Commands/GroupCommands/ImportStudentsCommand.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows.Forms;
+using EduPlatform.WPF.Commands.BaseCommands;
+using EduPlatform.WPF.Service.DataImport;
+using EduPlatform.WPF.Stores;
+using EduPlatform.WPF.ViewModels.GroupsViewModels;
+using OpenFileDialog = Microsoft.Win32.OpenFileDialog;
+
+namespace EduPlatform.WPF.Commands.GroupCommands;
+
+public class ImportStudentsCommand(StudentStore studentStore, GroupViewModel groupVM) : AsyncCommandBase
+{
+    public override async Task ExecuteAsync(object? parameter)
+    {
+        OpenFileDialog dialog = new()
+        {
+            Filter = "CSV Files (*.csv)|*.csv",
+            InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)
+        };
+
+        if (dialog.ShowDialog() == true)
+        {
+            string filePath = dialog.FileName;
+
+            CsvImporter importer = new();
+            await importer.ImportStudents(studentStore, groupVM, filePath);
+        }
+    }
+}
