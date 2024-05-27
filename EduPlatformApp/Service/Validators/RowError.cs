a769bfc35864a22c5f1a8412970a01bcf63fcaeb
HoliakaDmytro <holidim39@gmail.com>
2024-05-27 13:58:03 +0300

    Validators: Add class to present row error in csv file

---
+++ EduPlatformApp/Service/Validators/RowError.cs
@@ -0,0 +1,9 @@
+﻿namespace EduPlatform.WPF.Service.Validators;
+
+public class RowError(int lineNumber, string? columnName, string? explanation)
+{
+    public override string ToString()
+    {
+        return $"Row: {lineNumber}, column: «{columnName}». ({explanation})";
+    }
+}
