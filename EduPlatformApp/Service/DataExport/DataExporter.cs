0452ffdb7c4e15e00ad831a47b227d9141811947
HoliakaDmytro <holidim39@gmail.com>
2024-05-27 16:30:17 +0300

    DataExport: Change IDateExporter interface into abstract class and add method for getting filePath

---
+++ EduPlatformApp/Service/DataExport/DataExporter.cs
@@ -0,0 +1,21 @@
+﻿using EduPlatform.WPF.ViewModels.GroupsViewModels;
+using System.IO;
+
+namespace EduPlatform.WPF.Service.DataExport;
+
+public abstract class DataExporter
+{
+    public abstract Task ExportStudent(GroupViewModel groupVM);
+
+    protected string GetFilePath(string groupName, string extension)
+    {
+        string fileName = $"Students of {groupName}({DateTime.Now:yyyy.MM.dd hh-mm-ss.ffff}){extension}";
+        DirectoryInfo currentDir = new(AppDomain.CurrentDomain.BaseDirectory);
+
+        string dirPath = currentDir?.Parent?.Parent?.Parent?.Parent?.FullName + "/ExportedData/";
+        FileSystemManager.EnsureDirectoryCreated(dirPath);
+        string filePath = dirPath + "/" + fileName;
+
+        return filePath;
+    }
+}
