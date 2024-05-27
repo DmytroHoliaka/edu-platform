068e4a0e7c4008565dc6c6d60391528a11f83e4f
HoliakaDmytro <holidim39@gmail.com>
2024-05-27 16:33:14 +0300

    DataExport: Add dummy implementation of pdf export

---
+++ EduPlatformApp/Service/DataExport/PdfExporter.cs
@@ -0,0 +1,11 @@
+﻿using EduPlatform.WPF.ViewModels.GroupsViewModels;
+
+namespace EduPlatform.WPF.Service.DataExport;
+
+public class PdfExporter : DataExporter
+{
+    public override async Task ExportStudent(GroupViewModel groupVM)
+    {
+        throw new NotImplementedException();
+    }
+}
