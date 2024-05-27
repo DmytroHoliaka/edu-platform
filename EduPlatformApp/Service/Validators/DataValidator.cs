48fbd3d28c0f5722f3f42e156d873c2604e7ab0d
HoliakaDmytro <holidim39@gmail.com>
2024-05-27 13:59:00 +0300

    Validators: Add general data validator

---
+++ EduPlatformApp/Service/Validators/DataValidator.cs
@@ -0,0 +1,19 @@
+﻿namespace EduPlatform.WPF.Service.Validators;
+
+public abstract class DataValidator
+{
+    public static bool IsValidCorrect(string lineId)
+    {
+        if (int.TryParse(lineId, out int id) == false)
+        {
+            return false;
+        }
+
+        if (id < 0)
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
