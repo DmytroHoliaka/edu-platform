12e951838f7ccd295eef11222e6931a8afb914e7
HoliakaDmytro <holidim39@gmail.com>
2024-05-29 09:11:34 +0300

    GroupCommands: Add error handling in LoadGroupsCommand

--- EduPlatformApp/Commands/GroupCommands/LoadGroupsCommand.cs
+++ EduPlatformApp/Commands/GroupCommands/LoadGroupsCommand.cs
@@ -1,20 +1,22 @@
 ﻿using EduPlatform.WPF.Commands.BaseCommands;
 using EduPlatform.WPF.Stores;
+using EduPlatform.WPF.ViewModels.GroupsViewModels;
 
 namespace EduPlatform.WPF.Commands.GroupCommands
 {
-    public class LoadGroupsCommand(GroupStore groupStore) : AsyncCommandBase
+    public class LoadGroupsCommand(GroupStore groupStore, GroupSequenceViewModel groupSequenceViewModel) : AsyncCommandBase
     {
         public override async Task ExecuteAsync(object? parameter)
         {
+            groupSequenceViewModel.ErrorMessage = null;
+
             try
             {
                 await groupStore.Load();
             }
             catch (Exception)
             {
-                // ToDo: Write correct exception handling
-                throw;
+                groupSequenceViewModel.ErrorMessage = "Unable to load group data from database";
             }
         }
     }
