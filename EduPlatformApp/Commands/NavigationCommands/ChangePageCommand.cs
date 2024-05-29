472170e8919b720cd2a56a75cba5c4a07ee65c20
HoliakaDmytro <holidim39@gmail.com>
2024-05-29 09:53:19 +0300

    NavigationCommands: Add command as class to change page

---
+++ EduPlatformApp/Commands/NavigationCommands/ChangePageCommand.cs
@@ -0,0 +1,16 @@
+﻿using EduPlatform.WPF.Commands.BaseCommands;
+using EduPlatform.WPF.Service;
+using EduPlatform.WPF.ViewModels.NavigationsViewModels;
+
+namespace EduPlatform.WPF.Commands.NavigationCommands;
+
+public class ChangePageCommand(NavigationViewModel navigationViewModel) : CommandBase
+{
+    public override void Execute(object? parameter)
+    {
+        if (parameter is PageId pageId)
+        {
+            navigationViewModel.PageId = pageId;
+        }
+    }
+}
