40e1bad1a763d8dd6c4caa4eb68810c7e4853ae8
HoliakaDmytro <holidim39@gmail.com>
2024-05-29 09:13:04 +0300

    TeacherCommands: Add error handling in LoadTeachersCommand

--- EduPlatformApp/Commands/TeacherCommands/LoadTeachersCommand.cs
+++ EduPlatformApp/Commands/TeacherCommands/LoadTeachersCommand.cs
@@ -1,9 +1,11 @@
 ﻿using EduPlatform.WPF.Commands.BaseCommands;
 using EduPlatform.WPF.Stores;
+using EduPlatform.WPF.ViewModels.StudentsViewModels;
+using EduPlatform.WPF.ViewModels.TeachersViewModel;
 
 namespace EduPlatform.WPF.Commands.TeacherCommands
 {
-    public class LoadTeachersCommand(TeacherStore teacherStore) : AsyncCommandBase
+    public class LoadTeachersCommand(TeacherStore teacherStore, TeacherSequenceViewModel teacherSequenceViewModel) : AsyncCommandBase
     {
         public override async Task ExecuteAsync(object? parameter)
         {
@@ -13,8 +15,7 @@ namespace EduPlatform.WPF.Commands.TeacherCommands
             }
             catch (Exception)
             {
-                // ToDo: Write correct exception handling
-                throw;
+                teacherSequenceViewModel.ErrorMessage = "Unable to load student data from database";
             }
         }
     }
