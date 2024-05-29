516b62078c5718a48c5ea882867382605f5320c3
HoliakaDmytro <holidim39@gmail.com>
2024-05-29 16:15:43 +0300

    DataImport: Change signature of ImportStudents method in IDataImporter interface

--- EduPlatformApp/Service/DataImport/IDataImporter.cs
+++ EduPlatformApp/Service/DataImport/IDataImporter.cs
@@ -1,9 +1,10 @@
-﻿using EduPlatform.WPF.Stores;
+﻿using EduPlatform.Domain.Models;
+using EduPlatform.WPF.Stores;
 using EduPlatform.WPF.ViewModels.GroupsViewModels;
 
 namespace EduPlatform.WPF.Service.DataImport;
 
 public interface IDataImporter
 {
-    Task ImportStudents(StudentStore studentStore, GroupViewModel groupVM, string? path);
+    Task ImportStudents(StudentStore studentStore, Group? group, string? path);
 }
