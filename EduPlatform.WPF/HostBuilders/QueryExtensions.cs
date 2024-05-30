eeaf891038326d9ba34b49ccce57c79114cd4ed5
HoliakaDmytro <holidim39@gmail.com>
2024-05-30 09:46:34 +0300

    HostBuilders: Add QueryExtensions

---
+++ EduPlatform.WPF/HostBuilders/QueryExtensions.cs
@@ -0,0 +1,23 @@
+﻿using EduPlatform.Domain.Queries;
+using EduPlatform.EntityFramework.Queries;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace EduPlatform.WPF.HostBuilders;
+
+public static class QueryExtensions
+{
+    public static IHostBuilder AddQueries(this IHostBuilder hostBuilder)
+    {
+        hostBuilder.ConfigureServices(
+            (context, services) =>
+            {
+                services.AddSingleton<IGetAllCoursesQuery, GetAllCoursesQuery>();
+                services.AddSingleton<IGetAllGroupsQuery, GetAllGroupsQuery>();
+                services.AddSingleton<IGetAllStudentsQuery, GetAllStudentsQuery>();
+                services.AddSingleton<IGetAllTeachersQuery, GetAllTeachersQuery>();
+            });
+
+        return hostBuilder;
+    }
+}
