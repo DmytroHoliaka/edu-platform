b648f7bb64c64193457861755922d0a37be27c16
HoliakaDmytro <holidim39@gmail.com>
2024-05-30 08:51:33 +0300

    EntityFramework: Update EduPlatformDbContextDesignTimeFactory using created factory for IConfiguration

--- EduPlatform.EntityFramework/DbContextConfigurations/EduPlatformDbContextDesignTimeFactory.cs
+++ EduPlatform.EntityFramework/DbContextConfigurations/EduPlatformDbContextDesignTimeFactory.cs
@@ -1,14 +1,21 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Data;
+using EduPlatform.EntityFramework.Service;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
 
 namespace EduPlatform.EntityFramework.DbContextConfigurations
 {
+    // ToDo: Rename project EduPlatformApp -> EduPlatform.WPF
     public class EduPlatformDbContextDesignTimeFactory : IDesignTimeDbContextFactory<EduPlatformDbContext>
     {
         public EduPlatformDbContext CreateDbContext(string[] args)
         {
-            // ToDo: Use JSON configuration file #2
-            const string connectionString = @"Server=(localdb)\mssqllocaldb;Database=EduPlatform;Trusted_Connection=True;";
+            string connectionString = ConfigurationFactory
+                                          .GetConfiguration(jsonFileName: "appsettings.json")
+                                          .GetConnectionString(name: "MSSQL Server")
+                                      ?? throw new DataException("Cannot get connection string from json file");
+            
             DbContextOptions options = new DbContextOptionsBuilder()
                 .UseSqlServer(connectionString).Options;
             return new EduPlatformDbContext(options);
