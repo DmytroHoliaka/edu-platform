c1510ae21ce4bed610c0566b1fd13301cbfb4e2f
HoliakaDmytro <holidim39@gmail.com>
2024-05-30 09:46:51 +0300

    HostBuilders: Add ViewModelExtensions

---
+++ EduPlatform.WPF/HostBuilders/ViewModelExtensions.cs
@@ -0,0 +1,20 @@
+﻿using EduPlatform.WPF.ViewModels.GeneralViewModels;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace EduPlatform.WPF.HostBuilders;
+
+public static class ViewModelExtensions
+{
+    public static IHostBuilder AddViewModels(this IHostBuilder hostBuilder)
+    {
+        hostBuilder.ConfigureServices(
+            (context, services) =>
+            {
+                services.AddSingleton<HubViewModel>();
+                services.AddSingleton<MainViewModel>();
+            });
+
+        return hostBuilder;
+    }
+}
