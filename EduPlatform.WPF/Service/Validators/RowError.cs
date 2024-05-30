874dfa952d5990afdc0668a3d3fcf52cecb8902d
HoliakaDmytro <holidim39@gmail.com>
2024-05-30 17:36:47 +0300

    Validators: Rewrite RowError using explicit constructor

--- EduPlatform.WPF/Service/Validators/RowError.cs
+++ EduPlatform.WPF/Service/Validators/RowError.cs
@@ -1,9 +1,20 @@
 ﻿namespace EduPlatform.WPF.Service.Validators;
 
-public class RowError(int lineNumber, string? columnName, string? explanation)
+public class RowError
 {
+    public int LineNumber {get;}
+    public string? ColumnName {get;}
+    public string? Explanation { get; }
+
+    public RowError(int lineNumber, string? columnName, string? explanation)
+    {
+        LineNumber = lineNumber;
+        ColumnName = columnName;
+        Explanation = explanation;
+    }
+
     public override string ToString()
     {
-        return $"Row: {lineNumber}, column: «{columnName}». ({explanation})";
+        return $"Row: {LineNumber}, column: «{ColumnName}». ({Explanation})";
     }
 }
