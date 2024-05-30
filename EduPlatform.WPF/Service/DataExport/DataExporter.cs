666a4138044177ed4019a3154d107d4909da8ed0
HoliakaDmytro <holidim39@gmail.com>
2024-05-30 17:35:48 +0300

    DataExport: Add IClock to primary constructor and update GetFilePath method in DataExporter abstract class

--- EduPlatform.WPF/Service/DataExport/DataExporter.cs
+++ EduPlatform.WPF/Service/DataExport/DataExporter.cs
@@ -1,21 +1,21 @@
 ﻿using EduPlatform.WPF.Service.DataManagement;
 using EduPlatform.WPF.ViewModels.GroupsViewModels;
 using System.IO;
+using EduPlatform.WPF.Service.Time;
 
 namespace EduPlatform.WPF.Service.DataExport;
 
-public abstract class DataExporter
+public abstract class DataExporter(IClock clock)
 {
     public abstract Task ExportStudent(GroupViewModel groupVM);
 
-    protected string GetFilePath(string groupName, string extension)
+    protected string GetFilePath(string groupName, string extension, string folderPath)
     {
-        string fileName = $"Students of {groupName}({DateTime.Now:yyyy.MM.dd hh-mm-ss.ffff}){extension}";
+        string fileName = $"Students of {groupName}({clock.Now:yyyy.MM.dd hh-mm-ss.ffff}){extension}";
         DirectoryInfo currentDir = new(AppDomain.CurrentDomain.BaseDirectory);
 
-        string dirPath = currentDir.Parent?.Parent?.Parent?.Parent?.FullName + "/ExportedData/";
-        FileSystemManager.EnsureDirectoryCreated(dirPath);
-        string filePath = dirPath + "/" + fileName;
+        FileSystemManager.EnsureDirectoryCreated(folderPath);
+        string filePath = folderPath + "/" + fileName;
 
         return filePath;
     }
