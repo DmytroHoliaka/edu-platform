4280d73bb19d41189f3649cf694e3025253a9ff3
HoliakaDmytro <holidim39@gmail.com>
2024-05-30 09:47:15 +0300

    General: Decompose App.xaml.cs using extension methods

--- EduPlatform.WPF/App.xaml.cs
+++ EduPlatform.WPF/App.xaml.cs
@@ -9,6 +9,7 @@ using EduPlatform.WPF.Stores;
 using EduPlatform.WPF.ViewModels.GeneralViewModels;
 using Microsoft.EntityFrameworkCore;
 using System.Windows;
+using EduPlatform.WPF.HostBuilders;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -21,67 +22,25 @@ namespace EduPlatform.WPF
     /// </summary>
     public partial class App : Application
     {
-        private readonly IHost _host;
-
-        public App()
-        {
-            _host = Host.CreateDefaultBuilder()
-                .ConfigureServices(
-                    (context, services) =>
-                    {
-                        string connectionString = context.Configuration.GetConnectionString(name: "MSSQL Server")
-                            ?? throw new DataException("Cannot get connection string from configuration");
-
-                        services.AddSingleton<DbContextOptions>(
-                            new DbContextOptionsBuilder().UseSqlServer(connectionString).Options);
-                        services.AddSingleton<EduPlatformDbContextFactory>();
-
-                        services.AddSingleton<IGetAllCoursesQuery, GetAllCoursesQuery>();
-                        services.AddSingleton<ICreateCourseCommand, CreateCourseCommand>();
-                        services.AddSingleton<IUpdateCourseCommand, UpdateCourseCommand>();
-                        services.AddSingleton<IDeleteCourseCommand, DeleteCourseCommand>();
-
-                        services.AddSingleton<IGetAllGroupsQuery, GetAllGroupsQuery>();
-                        services.AddSingleton<ICreateGroupCommand, CreateGroupCommand>();
-                        services.AddSingleton<IUpdateGroupCommand, UpdateGroupCommand>();
-                        services.AddSingleton<IDeleteGroupCommand, DeleteGroupCommand>();
-
-                        services.AddSingleton<IGetAllStudentsQuery, GetAllStudentsQuery>();
-                        services.AddSingleton<ICreateStudentCommand, CreateStudentCommand>();
-                        services.AddSingleton<IUpdateStudentCommand, UpdateStudentCommand>();
-                        services.AddSingleton<IDeleteStudentCommand, DeleteStudentCommand>();
-
-                        services.AddSingleton<IGetAllTeachersQuery, GetAllTeachersQuery>();
-                        services.AddSingleton<ICreateTeacherCommand, CreateTeacherCommand>();
-                        services.AddSingleton<IUpdateTeacherCommand, UpdateTeacherCommand>();
-                        services.AddSingleton<IDeleteTeacherCommand, DeleteTeacherCommand>();
-
-                        services.AddSingleton<ModalNavigationStore>();
-                        services.AddSingleton<ViewStore>();
-
-                        services.AddSingleton<CourseStore>();
-                        services.AddSingleton<GroupStore>();
-                        services.AddSingleton<StudentStore>();
-                        services.AddSingleton<TeacherStore>();
-                        services.AddSingleton<HubViewModel>();
-
-                        services.AddSingleton<MainViewModel>();
-                        services.AddSingleton<MainWindow>(serviceProvider => new MainWindow()                        
-                        {
-                            DataContext = serviceProvider.GetRequiredService<MainViewModel>()
-                        });
-                    })
+        private readonly IHost _host =
+            Host.CreateDefaultBuilder()
+                .AddDbContext()
+                .AddQueries()
+                .AddCommands()
+                .AddStores()
+                .AddViewModels()
+                .AddViews()
                 .Build();
-        }
 
         protected override async void OnStartup(StartupEventArgs e)
         {
             _host.Start();
 
-            EduPlatformDbContextFactory contextFactory = _host.Services.GetRequiredService<EduPlatformDbContextFactory>();
+            EduPlatformDbContextFactory contextFactory =
+                _host.Services.GetRequiredService<EduPlatformDbContextFactory>();
             MainWindow mainWindow = _host.Services.GetRequiredService<MainWindow>();
             HubViewModel hubVM = _host.Services.GetRequiredService<HubViewModel>();
-            
+
             using (EduPlatformDbContext context = contextFactory.Create())
             {
                 await context.Database.MigrateAsync();
