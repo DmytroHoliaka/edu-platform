16af560c8b3a5fbebab9cf87037019bd0dcb003f
HoliakaDmytro <holidim39@gmail.com>
2024-05-30 17:40:03 +0300

    Fixtures: Add fixture class for DocxExporter

---
+++ EduPlatform.WPF.Tests.UnitTesting.xUnit/Fixtures/DocxExporterFixtures.cs
@@ -0,0 +1,30 @@
+﻿using EduPlatform.WPF.Service.Time;
+using Moq;
+
+namespace EduPlatform.WPF.Tests.UnitTesting.xUnit.Fixtures;
+
+public class DocxExporterFixtures : IDisposable
+{
+    public string DirPath { get; }
+    public Mock<IClock> Mock { get; }
+
+    public DocxExporterFixtures()
+    {
+        Mock = new Mock<IClock>();
+        Mock.Setup(clock => clock.Now).Returns(new DateTime(2024, 9, 24, 11, 45, 12, 250));
+
+        DirPath = "./DocxExporterFixturesTest";
+
+        if (Directory.Exists(path: DirPath))
+        {
+            Directory.Delete(path: DirPath, recursive: true);
+        }
+
+        Directory.CreateDirectory(path: DirPath);
+    }
+
+    public void Dispose()
+    {
+        Directory.Delete(path: DirPath, recursive: true);
+    }
+}
