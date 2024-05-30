6778fe6a7800594ee9c02769e6355c2ed0d42f43
HoliakaDmytro <holidim39@gmail.com>
2024-05-30 09:46:08 +0300

    HostBuilders: Add CommandExtensions

---
+++ EduPlatform.WPF/HostBuilders/CommandExtensions.cs
@@ -0,0 +1,38 @@
+﻿using DocumentFormat.OpenXml.Office2016.Drawing.ChartDrawing;
+using EduPlatform.Domain.Commands;
+using EduPlatform.EntityFramework.Commands;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace EduPlatform.WPF.HostBuilders;
+
+public static class CommandExtensions
+{
+    public static IHostBuilder AddCommands(this IHostBuilder hostBuilder)
+    {
+        hostBuilder.ConfigureServices(
+            (context, services) =>
+            {
+                services.AddSingleton<ICreateCourseCommand, CreateCourseCommand>();
+                services.AddSingleton<IUpdateCourseCommand, UpdateCourseCommand>();
+                services.AddSingleton<IDeleteCourseCommand, DeleteCourseCommand>();
+
+
+                services.AddSingleton<ICreateGroupCommand, CreateGroupCommand>();
+                services.AddSingleton<IUpdateGroupCommand, UpdateGroupCommand>();
+                services.AddSingleton<IDeleteGroupCommand, DeleteGroupCommand>();
+
+
+                services.AddSingleton<ICreateStudentCommand, CreateStudentCommand>();
+                services.AddSingleton<IUpdateStudentCommand, UpdateStudentCommand>();
+                services.AddSingleton<IDeleteStudentCommand, DeleteStudentCommand>();
+
+
+                services.AddSingleton<ICreateTeacherCommand, CreateTeacherCommand>();
+                services.AddSingleton<IUpdateTeacherCommand, UpdateTeacherCommand>();
+                services.AddSingleton<IDeleteTeacherCommand, DeleteTeacherCommand>();
+            });
+
+        return hostBuilder;
+    }
+}
