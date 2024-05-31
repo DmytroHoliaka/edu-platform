cf9b5a2bc584c4a30f9a33652a0d851cf24485f2
HoliakaDmytro <holidim39@gmail.com>
2024-05-31 13:23:54 +0300

    Fixtures: Add database class fixture

---
+++ EduPlatform.EntityFramework.Tests.UnitTesting.xUnit/Fixtures/DatabaseFixture.cs
@@ -0,0 +1,40 @@
+﻿using EduPlatform.EntityFramework.DbContextConfigurations;
+using EduPlatform.EntityFramework.Service;
+using Microsoft.EntityFrameworkCore;
+
+namespace EduPlatform.EntityFramework.Tests.UnitTesting.xUnit.Fixtures;
+// ToDo: Check that creates only one test context for all DatabaseTests collection
+public class DatabaseFixture : IDisposable
+{
+    public EduPlatformDbContextFactory DbContextFactory { get; }
+    public DatabaseManager DbManager { get; }
+
+    private const string ConnectionLine =
+        "Server=(localdb)\\mssqllocaldb;" +
+        "Database=EduPlatformTests;" +
+        "Trusted_Connection=True;";
+
+    public DatabaseFixture()
+    {
+        DbContextOptionsBuilder builder = new();
+        builder.UseSqlServer(ConnectionLine);
+        DbContextOptions options = builder.Options;
+
+        DbContextFactory = new EduPlatformDbContextFactory(options);
+        DbManager = new DatabaseManager(DbContextFactory);
+
+        using (EduPlatformDbContext context = DbContextFactory.Create())
+        {
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+        }
+    }
+
+    public void Dispose()
+    {
+        using (EduPlatformDbContext context = DbContextFactory.Create())
+        {
+            context.Database.EnsureDeleted();
+        }
+    }
+}
