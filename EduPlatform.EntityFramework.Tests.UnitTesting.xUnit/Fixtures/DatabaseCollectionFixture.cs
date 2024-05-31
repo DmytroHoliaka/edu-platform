8465cb68333f9244a45ae1a932e8cdbf9e4a0495
HoliakaDmytro <holidim39@gmail.com>
2024-05-31 13:24:14 +0300

    Fixtures: Add fixture for implementing collection fixtures

---
+++ EduPlatform.EntityFramework.Tests.UnitTesting.xUnit/Fixtures/DatabaseCollectionFixture.cs
@@ -0,0 +1,9 @@
+﻿using Xunit;
+
+namespace EduPlatform.EntityFramework.Tests.UnitTesting.xUnit.Fixtures;
+
+[CollectionDefinition("DatabaseTests")]
+public class DatabaseCollectionFixture : ICollectionFixture<DatabaseFixture>
+{
+    // Empty class to implement collection of fixtures
+}
