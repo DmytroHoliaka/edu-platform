2236be1d2676edddc7d5d9fd71b43e9daaf5a31a
HoliakaDmytro <holidim39@gmail.com>
2024-05-31 13:23:31 +0300

    Comparators: Add a comparator for comparing students by ids

---
+++ EduPlatform.EntityFramework.Tests.UnitTesting.xUnit/Comparers/EntityIdEqualityComparers/StudentIdEqualityComparer.cs
@@ -0,0 +1,26 @@
+﻿using EduPlatform.Domain.Models;
+
+namespace EduPlatform.EntityFramework.Tests.UnitTesting.xUnit.Comparers.EntityIdEqualityComparers;
+
+public class StudentIdEqualityComparer : IEqualityComparer<Student>
+{
+    public bool Equals(Student? x, Student? y)
+    {
+        if (x == y)
+        {
+            return true;
+        }
+
+        if (x is null || y is null)
+        {
+            return false;
+        }
+
+        return x.StudentId == y.StudentId;
+    }
+
+    public int GetHashCode(Student obj)
+    {
+        return HashCode.Combine(obj.StudentId);
+    }
+}
